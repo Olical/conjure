bef8831494fd0dcdb7c29b2ac6c6b9f92378a5e5
Oliver Caldwell <olliec87@gmail.com>
2018-10-31 13:37:18 +0000

    Slowly working out what types / traits I want
    I want it to be generic, so a client gives you a way to write as well as
    a way to get responses out. Right now I think I just want return, out
    and tap. Just encoding the pREPL. Maybe I don't make it generic from the
    start...

--- src/client.rs
+++ src/client.rs
@@ -4,6 +4,32 @@ use std::io;
 use std::io::prelude::*;
 use std::net;
 
+enum Value {
+    Return,
+    Out,
+    Tap,
+    Error,
+}
+
+// {:tag :ret
+//  :val val ;;eval result
+//  :ns ns-name-string
+//  :ms long ;;eval time in milliseconds
+//  :form string ;;iff successfully read
+// }
+// {:tag :out
+//  :val string} ;chars from during-eval *out*
+// {:tag :err
+//  :val string} ;chars from during-eval *err*
+// {:tag :tap
+//  :val val} ;values from tap>
+
+trait Client {
+    fn new(addr: &'static str) -> Self;
+    fn write(&mut self, &str) -> io::Result<usize>;
+    fn iter(&mut self) -> Iterator<Item = Result<edn::Value, edn::parser::Error>>;
+}
+
 pub fn start() {
     if let Ok(mut stream) = net::TcpStream::connect("127.0.0.1:5555") {
         println!("Connected!");
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,2 @@
-pub mod prepl;
+pub mod client;
 pub mod server;
-
-use std::thread;
-
-pub fn start() {
-    let handles = vec![
-        thread::spawn(|| {
-            server::start();
-        }),
-        thread::spawn(|| {
-            prepl::start();
-        }),
-    ];
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-}
