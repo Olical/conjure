b598e196214a9df5b625c8db238373179457fd3e
Oliver Caldwell <olliec87@gmail.com>
2018-11-06 23:40:42 +0000

    Remove a random 'static

--- src/client.rs
+++ src/client.rs
@@ -54,7 +54,7 @@ pub struct Client {
 }
 
 impl Client {
-    pub fn connect(addr: &'static str) -> Result<Client, String> {
+    pub fn connect(addr: &str) -> Result<Client, String> {
         let raw_stream = TcpStream::connect(addr)
             .map_err(|msg| format!("Couldn't connect to `{}`: {}", addr, msg))?;
 
