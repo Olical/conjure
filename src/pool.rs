750a4559b58efc1976da045f90027ab664fe03ef
Oliver Caldwell <olliec87@gmail.com>
2018-11-21 14:54:25 +0000

    Add a couple more todos I thought of

--- src/pool.rs
+++ src/pool.rs
@@ -10,6 +10,8 @@ use util;
 // TODO What if someone sends :repl/quit?
 // TODO What if a REPL dies?
 // TODO Pass paths to Clojure for it to look up namespaces and in-ns on them.
+// TODO Maybe use pst to print the stack trace?
+// TODO Show some sort of placeholder while evaling.
 
 pub struct Connection {
     user: Client,
