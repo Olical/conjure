5d4c6bfe3317a32ec91eb508101ba4651048739f
Oliver Caldwell <olliec87@gmail.com>
2019-01-23 12:40:02 +0000

    Reinstate the ns fn in util

--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,20 @@
 use regex::Regex;
-use std::io::prelude::*;
 
 pub fn escape_quotes(s: &str) -> String {
     s.replace("\\", "\\\\").replace("\"", "\\\"")
+}
+
+pub fn ns(source: &str) -> Option<String> {
+    lazy_static! {
+        static ref ns_re: Regex = Regex::new(r"\(\s*ns\s+(\D[[[:word:]]\.\*\+!\-'?]*)\s*")
+            .expect("failed to compile ns regex");
+    }
+
+    if let Some(cap) = ns_re.captures_iter(source).next() {
+        cap.get(1).map(|ns| ns.as_str().to_owned())
+    } else {
+        None
+    }
 }
 
 pub fn parse_location(loc_str: &str) -> Option<(String, i64, i64)> {
@@ -57,5 +69,17 @@ mod tests {
     fn escaping_quotes() {
         assert_eq!(escape_quotes("'foo'"), "'foo'");
         assert_eq!(escape_quotes("\"foo\""), "\\\"foo\\\"");
+    }
+
+    #[test]
+    fn parsing_a_ns() {
+        assert_eq!(ns("(ns foo.my-ns)").unwrap(), "foo.my-ns");
+        assert_eq!(ns("(ns foo.my-ns))").unwrap(), "foo.my-ns");
+        assert_eq!(ns("(ns foo.my-ns \"docs\") :boop").unwrap(), "foo.my-ns");
+        assert!(ns("nope").is_none());
+        assert_eq!(
+            ns("( \n\n \n ns \n\n   \n foo__123.my-ns!?. \n\"lol docs?\" ¯\\_(ツ)_/¯)").unwrap(),
+            "foo__123.my-ns!?."
+        );
     }
 }
