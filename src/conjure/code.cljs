a03260265143a190d9fd3ae2ef9512d25e5a1f1a
Oliver Caldwell <olliec87@gmail.com>
2019-02-27 23:33:35 +0000

    Wrap up zprint _just in case_ it has a bug
    So the code will still flow through at least.

--- src/conjure/code.cljs
+++ src/conjure/code.cljs
@@ -1,9 +1,14 @@
 (ns conjure.code
   "Generation and manipulation of all Clojure and ClojureScript."
-  (:require [zprint.core :as zp]))
+  (:require [zprint.core :as zp]
+            [conjure.display :as display]))
 
 (defn pretty-print [s]
-  (zp/zprint-str (str s)
-                 {:parse-string-all? true
-                  :parse {:interpose "\n\n"}}))
+  (try
+    (zp/zprint-str (str s)
+                   {:parse-string-all? true
+                    :parse {:interpose "\n\n"}})
+    (catch :default e
+      (display/error! nil e)
+      s)))
 
