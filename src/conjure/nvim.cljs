d9aaa40fd556a28f15bec6b74ec4626de36c4a2e
Oliver Caldwell <olliec87@gmail.com>
2019-02-27 23:15:38 +0000

    More nvim functions

--- src/conjure/nvim.cljs
+++ src/conjure/nvim.cljs
@@ -1,6 +1,7 @@
 (ns conjure.nvim
   "Wrapper around all nvim functions."
   (:require [cljs.nodejs :as node]
+            [cljs.core.async :as a]
             [clojure.string :as str]
             [applied-science.js-interop :as j]
             [conjure.util :as util]))
@@ -16,16 +17,39 @@
   (reset! plugin! plugin)
   (reset! api! (j/get plugin :nvim)))
 
+(defn- join [args]
+  (str/join " " (remove nil? args)))
+
 (defn <buffer []
   (-> (j/get @api! :buffer)
+      (util/->chan)))
+
+(defn <window []
+  (-> (j/get @api! :window)
       (util/->chan)))
 
 (defn <path [buffer]
   (-> (j/get buffer :name)
       (util/->chan)))
 
-(defn- join [args]
-  (str/join " " (remove nil? args)))
+(defn <length [buffer]
+  (-> (j/get buffer :length)
+      (util/->chan)))
+
+(defn append! [buffer & args]
+  (j/call buffer :append (join args)))
+
+(defn set-width! [window width]
+  (j/assoc! window :width width))
+
+(defn set-cursor! [window {:keys [x y]}]
+  (j/assoc! window :cursor #js [y x]))
+
+(defn scroll-to-bottom! [window]
+  (a/go
+    (let [buffer (a/<! (<buffer))
+          length (a/<! (<length buffer))]
+      (set-cursor! window {:x 0, :y length}))))
 
 (defn out-write! [& args]
   (j/call @api! :outWrite (join args)))
