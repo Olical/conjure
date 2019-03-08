4d1154070dcfa54f9402539401e335428cd2d039
Oliver Caldwell <olliec87@gmail.com>
2019-03-08 18:16:48 +0000

    Oops, log/info, not log/info!

--- src/conjure/main.clj
+++ src/conjure/main.clj
@@ -8,7 +8,7 @@
   (dev/init!)
   (rpc/init!)
 
-  (log/info! "Everything's up and running")
+  (log/info "Everything's up and running")
 
   (let [fry (a/chan)]
     ;; https://www.youtube.com/watch?v=6UHlXLmsDGA
