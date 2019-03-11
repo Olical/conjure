ac38328f6e52ca45a96a738d1ee9d2e68cb2e071
Oliver Caldwell <olliec87@gmail.com>
2019-03-11 13:27:38 +0000

    Progress on the connect fn, more to do!

--- src/conjure/pool.clj
+++ src/conjure/pool.clj
@@ -5,11 +5,8 @@
             [clojure.core.server :as server]
             [clojure.java.io :as io]
             [taoensso.timbre :as log]
-            [conjure.util :as util]))
-
-(let [s (java.io.StringWriter.)]
-  (.write s "foo" 0 3)
-  (io/reader s))
+            [conjure.util :as util])
+  (:import [java.io PipedInputStream PipedOutputStream]))
 
 (s/def ::expr util/regexp?)
 (s/def ::tag keyword?)
@@ -19,20 +16,42 @@
 (s/def ::new-conn (s/keys :req-un [::tag ::port]
                           :opt-un [::expr ::lang ::host]))
 
-(defonce conns! (atom {}))
-
-(defn connect [{:keys [host port]}]
-  (let [[eval-chan read-chan aux-chan] (repeatedly a/chan)]
-    (server/remote-prepl host port)))
-
 (def default-exprs
   {:clj #"\.cljc?$"
    :cljs #"\.clj(s|c)$"})
 
+(defonce conns! (atom {}))
+
 (defn remove! [tag]
   (when-let [conn (get @conns! tag)]
     (log/info "Removing:" conn)
-    (swap! conns! dissoc tag)))
+    (swap! conns! dissoc tag)
+    (doseq [c (vals (:prepl conn))]
+      (a/close! c))))
+
+;; TODO Ensure the prepl thread is killed (future?) when removed
+;; TODO Ensure we remove everything else if the prepl dies
+(defn connect [{:keys [tag host port]}]
+  (let [[eval-chan read-chan aux-chan] (repeatedly a/chan)
+        input (PipedInputStream.)
+        output (PipedOutputStream. input)]
+
+    (a/thread
+      (with-open [in-reader (io/reader output)]
+        (server/remote-prepl host port in-reader
+                             (fn [{:keys [tag] :as out}]
+                               (a/>!! (if (= tag :eval) read-chan aux-chan) out))))
+      (remove! tag))
+
+    (a/thread
+      (loop []
+        (when-let [code (a/<!! eval-chan)]
+          (.write input code 0 (count code))
+          (recur))))
+
+    {:eval-chan eval-chan
+     :read-chan read-chan
+     :aux-chan aux-chan}))
 
 (defn add! [{:keys [tag port lang expr host]
              :or {tag :default
