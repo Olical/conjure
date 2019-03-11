19c2a01222b407900eea27f64bd17f65268e2978
Oliver Caldwell <olliec87@gmail.com>
2019-03-11 12:54:57 +0000

    Shuffle main about

--- src/conjure/main.clj
+++ src/conjure/main.clj
@@ -9,19 +9,6 @@
             [conjure.pool :as pool]
             [conjure.ui :as ui]))
 
-;; So users can pass {:expr #regex "..."}
-;; EDN doesn't support regular expressions out of the box.
-(def edn-opts {:readers {'regex re-pattern}})
-
-(defn parse-user-edn
-  "Parses some string as EDN and ensures it conforms to a spec.
-  Returns nil and displays an error if it fails."
-  [spec src]
-  (let [value (edn/read-string edn-opts src)]
-    (if (s/valid? spec value)
-      value
-      (ui/error (expound/expound-str spec value)))))
-
 (defn -main
   "Start up any background services and then wait forever."
   []
@@ -36,6 +23,19 @@
   ;; /,    /`
   ;; \\"--\\
   @(promise))
+
+;; So users can pass {:expr #regex "..."}
+;; EDN doesn't support regular expressions out of the box.
+(def edn-opts {:readers {'regex re-pattern}})
+
+(defn parse-user-edn
+  "Parses some string as EDN and ensures it conforms to a spec.
+  Returns nil and displays an error if it fails."
+  [spec src]
+  (let [value (edn/read-string edn-opts src)]
+    (if (s/valid? spec value)
+      value
+      (ui/error (expound/expound-str spec value)))))
 
 ;; Here we map RPC notifications and requests to their Clojure functions.
 ;; Input strings are parsed as EDN and checked against specs where required.
