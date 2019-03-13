0ad7cd7f1ef3feed68edaeef512ccf4e6aef6eac
Oliver Caldwell <olliec87@gmail.com>
2019-03-13 13:02:38 +0000

    Log errors from nvim

--- src/conjure/nvim.clj
+++ src/conjure/nvim.clj
@@ -1,13 +1,15 @@
 (ns conjure.nvim
   "Tools to interact with Neovim at a higher level than RPC."
-  (:require [conjure.rpc :as rpc]))
+  (:require [taoensso.timbre :as log]
+            [conjure.rpc :as rpc]))
 
 ;; TODO Handle batch calls
-;; TODO Handle errors nicely (do I throw?)
 (defn call
   "Simply a thin nvim specific wrapper around rpc/request."
   [req]
-  (let [{:keys [error result]} (rpc/request req)]
+  (let [{:keys [error result] :as resp} (rpc/request req)]
+    (when error
+      (log/error "Error while making nvim call" req "->" resp))
     result))
 
 (defn get-current-buf []
