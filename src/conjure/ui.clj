6ef70fe3dd6c1b87e66637b1d231be4904a32f8c
Oliver Caldwell <olliec87@gmail.com>
2019-03-15 16:07:56 +0000

    Pull reusable code into append

--- src/conjure/ui.clj
+++ src/conjure/ui.clj
@@ -10,6 +10,7 @@
 (def max-log-buffer-length 10000)
 (defonce log-buffer-name (str "/tmp/conjure-log-" (util/now) ".cljc"))
 (def upsert-log-lua "return conjure_utils.upsert_log(...)")
+(def welcome-msg ";conjure/out Welcome to Conjure!")
 
 (defn upsert-log
   "Get, create, or update the log window and buffer."
@@ -23,20 +24,24 @@
         (nvim/call)
         (util/snake->kw-map))))
 
-(defn error [msg]
-  (let [lines (for [line (util/lines msg)]
-                (str ";conjure/err " line))
+(defn append [{:keys [src msg code?] :or {code? false}}]
+  (let [prefix (str ";" (namespace src) "/" (name src))
+        lines (if code?
+                (str prefix "\n" msg)
+                (for [line (util/lines msg)]
+                  (str prefix " " line)))
         {:keys [buf win]} (upsert-log)
         line-count (nvim/call (nvim/buf-line-count buf))
         new-line-count (+ line-count (count lines))]
 
     (nvim/call-batch
       [(when (= line-count 1)
-         (nvim/buf-set-lines buf
-                             {:start 0, :end 1}
-                             [";conjure/out Welcome to Conjure!"]))
+         (nvim/buf-set-lines buf {:start 0, :end 1} [welcome-msg]))
        (nvim/buf-set-lines buf {:start -1, :end -1} lines)
        (nvim/win-set-cursor win {:col 0, :row new-line-count})])))
+
+(defn error [msg]
+  (append {:src :conjure/err, :msg msg}))
 
 (comment
   (time (error "Henlo, World!\nConjure is magic.")))
