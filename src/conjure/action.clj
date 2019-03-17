a303ba4ca093df7026216ade3957d2aef6531a55
Oliver Caldwell <olliec87@gmail.com>
2019-03-17 13:22:05 +0000

    Optimise evals by building ctx once

--- src/conjure/action.clj
+++ src/conjure/action.clj
@@ -7,54 +7,48 @@
             [conjure.nvim :as nvim]
             [conjure.code :as code]))
 
-;; When looking for the current ns we only ask for this many lines.
-;; This avoids pulling the entire file over RPC upon every action.
-(def current-ns-sample-count 25)
-
-(defn- current-path []
-  (-> (nvim/get-current-buf) (nvim/call)
-      (nvim/buf-get-name) (nvim/call)))
-
-(defn- relevant-conns []
-  (let [path (current-path)]
-    (if-let [conns (pool/conns path)]
-      conns
-      (ui/error (ui/error "No matching connections for" path)))))
-
-(defn- current-ns []
-  (let [sample (-> (nvim/get-current-buf) (nvim/call)
-                   (nvim/buf-get-lines
-                     {:start 0
-                      :end current-ns-sample-count}) (nvim/call)
-                   (->> (str/join "\n")))]
-    (code/extract-ns sample)))
+(defn- current-ctx
+  "Context contains useful data that we don't watch to fetch twice while
+  building code to eval. This function performs those costly calls."
+  []
+  (let [buf (-> (nvim/get-current-buf) (nvim/call))
+        [path sample-lines]
+        (nvim/call-batch
+          [(nvim/buf-get-name buf)
+           (nvim/buf-get-lines buf {:start 0, :end 25})])
+        conns (pool/conns path)]
+    {:path path
+     :ns (code/extract-ns (str/join "\n" sample-lines))
+     :conns (or conns (ui/error "No matching connections for" path))}))
 
 (defn eval* [code]
-  (let [ns (current-ns)]
-    (when-let [conns (relevant-conns)]
-      (doseq [{:keys [chans] :as conn} conns]
-        (ui/eval* {:conn conn, :code code})
-        (a/>!! (:eval-chan chans) (code/eval-str {:conn conn
-                                                  :ns ns
-                                                  :code code}))
-        (ui/result {:conn conn, :resp (a/<!! (:ret-chan chans))})))))
+  (let [ctx (current-ctx)]
+    (doseq [{:keys [chans] :as conn} (:conns ctx)]
+      (ui/eval* {:conn conn, :code code})
+      (a/>!! (:eval-chan chans) (code/eval-str (merge ctx {:conn conn
+                                                           :code code})))
+      (ui/result {:conn conn, :resp (a/<!! (:ret-chan chans))}))))
 
 (defn doc [name]
-  (let [ns (current-ns)]
-    (when-let [conns (relevant-conns)]
-      (doseq [{:keys [chans] :as conn} conns]
-        (a/>!! (:eval-chan chans) (code/doc-str {:conn conn
-                                                 :ns ns
-                                                 :name name}))
-        (let [resp (a/<!! (:ret-chan chans))]
-          (ui/doc {:conn conn
-                   :resp (cond-> resp
-                           (empty? (:val resp))
-                           (assoc :val (str "No doc for " name)))}))))))
+  (let [ctx (current-ctx)]
+    (doseq [{:keys [chans] :as conn} (:conns ctx)]
+      (a/>!! (:eval-chan chans) (code/doc-str (merge ctx {:conn conn
+                                                          :name name})))
+      (let [resp (a/<!! (:ret-chan chans))]
+        (ui/doc {:conn conn
+                 :resp (cond-> resp
+                         (empty? (:val resp))
+                         (assoc :val (str "No doc for " name)))})))))
 
 (comment
   (pool/conns)
-  (pool/add! {:port 5555})
+  (pool/add! {:tag :dev-jvm
+              :port 5555
+              :lang :clj})
+  (pool/add! {:tag :dev-node
+              :port 5556
+              :lang :cljs
+              :expr #"\.cljc?$"})
   (time (eval* "(prn 1) (prn 2)"))
   (time (eval* "#?(:clj 1, :cljs 2)"))
   (time (doc "+"))
