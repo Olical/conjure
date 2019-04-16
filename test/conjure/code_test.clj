0228b93303e082c9b15d488762332a4f2d5c0eeb
Oliver Caldwell <ollie@oli.me.uk>
2019-04-16 22:53:40 +0100

    Add a few more simple tests

---
+++ test/conjure/code_test.clj
@@ -0,0 +1,15 @@
+(ns conjure.code-test
+  (:require [clojure.test :as t]
+            [conjure.code :as code]))
+
+(t/deftest pprint
+  (t/is (= (code/pprint "#?(:clj :some-code)") "#?(:clj :some-code)")))
+
+(t/deftest sample
+  (t/is (= (code/sample "this is some code") "this is some code"))
+  (t/is (= (code/sample "this is some long code and it exceeds the character limit")
+           "this is some long code and it exceeds the …")))
+
+(t/deftest extract-ns
+  (t/is (= (code/extract-ns "lol nope") nil))
+  (t/is (= (code/extract-ns "(ns some.ns-woo)") "some.ns-woo")))
--- test/conjure/util_test.clj
+++ test/conjure/util_test.clj
@@ -3,5 +3,17 @@
             [conjure.util :as util]))
 
 (t/deftest join-words
+  (t/is (= (util/join-words nil) ""))
   (t/is (= (util/join-words []) ""))
   (t/is (= (util/join-words ["foo" "bar"]) "foo bar")))
+
+(t/deftest escape-quotes
+  (t/is (= (util/escape-quotes "\"\"") "\\\"\\\"")))
+
+(t/deftest count-str
+  (t/is (= (util/count-str [] "number") "0 numbers"))
+  (t/is (= (util/count-str [1] "number") "1 number"))
+  (t/is (= (util/count-str [1 2] "number") "2 numbers")))
+
+(t/deftest free-port
+  (t/is (number? (util/free-port))))
