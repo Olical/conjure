cdcba88104c050383d2961fc59d0cb246c3e484c
Oliver Caldwell <ollie@oli.me.uk>
2019-04-22 17:27:35 +0100

    Better indenting

--- test/conjure/nvim_test.clj
+++ test/conjure/nvim_test.clj
@@ -4,12 +4,11 @@
             [conjure.nvim.api :as api]))
 
 (t/deftest current-ctx
-  (with-redefs
-    [api/call (constantly 5)
-     api/call-batch (constantly
-                      ["foo.clj"
-                       ["(ns foo)"]
-                       10])]
+  (with-redefs [api/call (constantly 5)
+                api/call-batch (constantly
+                                 ["foo.clj"
+                                  ["(ns foo)"]
+                                  10])]
     (t/is (= (nvim/current-ctx)
              {:path "foo.clj"
               :buf 5
