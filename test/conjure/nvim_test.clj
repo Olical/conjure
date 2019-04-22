d07d1314db6adca88bb8b040baff35bf4d27706b
Oliver Caldwell <ollie@oli.me.uk>
2019-04-22 22:13:10 +0100

    Working on mocking RPC calls

--- test/conjure/nvim_test.clj
+++ test/conjure/nvim_test.clj
@@ -1,16 +1,32 @@
 (ns conjure.nvim-test
   (:require [clojure.test :as t]
             [conjure.nvim :as nvim]
-            [conjure.nvim.api :as api]))
+            [conjure.rpc :as rpc]))
 
-(t/deftest current-ctx
-  (with-redefs [api/call (constantly 5)
-                api/call-batch (constantly
-                                 ["foo.clj"
-                                  ["(ns foo)"]
-                                  10])]
+(defmulti request :method)
+
+(t/use-fixtures
+  :each
+  (fn [f]
+    (try
+      (defmethod request :default [{:keys [method] :as req}]
+        (throw (ex-info (str "Unhandled request: " method) req)))
+
+      (defmethod request :nvim-call-atomic [{:keys [params]}]
+        (map (fn [{:keys [method params]}]
+               (request {:method method, :params params}))
+             params))
+
+      (binding [rpc/request request]
+        (f))
+      (finally
+        (remove-all-methods request)))))
+
+#_(t/deftest current-ctx
+    (defmethod request :nvim-get-current-buf [_] 5)
+
     (t/is (= (nvim/current-ctx)
              {:path "foo.clj"
               :buf 5
               :win 10
-              :ns 'foo}))))
+              :ns 'foo})))
