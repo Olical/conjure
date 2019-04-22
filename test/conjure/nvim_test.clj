d35367b004d15a364497e6153272db0854ac53e1
Oliver Caldwell <ollie@oli.me.uk>
2019-04-22 17:24:57 +0100

    Add a tiny test of current-ctx
    I think I need a nice way to stub RPC requests and responses.

---
+++ test/conjure/nvim_test.clj
@@ -0,0 +1,17 @@
+(ns conjure.nvim-test
+  (:require [clojure.test :as t]
+            [conjure.nvim :as nvim]
+            [conjure.nvim.api :as api]))
+
+(t/deftest current-ctx
+  (with-redefs
+    [api/call (constantly 5)
+     api/call-batch (constantly
+                      ["foo.clj"
+                       ["(ns foo)"]
+                       10])]
+    (t/is (= (nvim/current-ctx)
+             {:path "foo.clj"
+              :buf 5
+              :win 10
+              :ns 'foo}))))
