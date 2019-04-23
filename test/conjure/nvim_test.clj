eb589bd994d6367f3648b3c8773cb7b937130b45
Oliver Caldwell <ollie@oli.me.uk>
2019-04-23 22:58:32 +0100

    Starting to test read-form, not there yet

--- test/conjure/nvim_test.clj
+++ test/conjure/nvim_test.clj
@@ -1,5 +1,6 @@
 (ns conjure.nvim-test
   (:require [clojure.test :as t]
+            [clojure.string :as str]
             [conjure.nvim :as nvim]
             [conjure.nvim.api :as api]))
 
@@ -33,3 +34,29 @@
             :buf 5
             :win 10
             :ns 'foo})))
+
+#_
+(t/deftest read-form
+  (defmethod call :nvim-get-current-buf [_] 5)
+  (defmethod call :nvim-get-current-win [_] 10)
+  (defmethod call :nvim-eval [_] "e")
+  (defmethod call :nvim-call-function [{[_ [s _ e args]] :params}]
+    (get {["()" :b] [2 16]
+          ["()" :f] [2 30]}
+         [(str s e)
+          (if (str/starts-with? args "b")
+            :b
+            :f)]
+         [0 0]))
+  (defmethod call :nvim-win-get-cursor [{[win] :params}]
+    (t/is (= win 10))
+    [2 18])
+  (defmethod call :nvim-buf-get-lines [{[buf start end] :params}]
+    (t/is (= buf 5))
+    (t/is (= start 1))
+    (t/is (= end 2))
+    ["(+ 10 10)"
+     "[:foo] {:x :y} (hello (world)) [:bar]"
+     ":hello"])
+
+  (t/is (= (nvim/read-form) "(hello (world))")))
