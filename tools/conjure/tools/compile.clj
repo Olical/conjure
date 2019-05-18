c63372792d74ab75b40b3571861fafdc870abbff
Oliver Caldwell <ollie@oli.me.uk>
2019-05-18 17:55:13 +0100

    Mr Andersonified compliment!

--- tools/conjure/tools/compile.clj
+++ tools/conjure/tools/compile.clj
@@ -1,4 +1,23 @@
-(ns conjure.tools.compile)
+(ns conjure.tools.compile
+  (:require [clojure.java.io :as io]
+            [mranderson.core :as ma]))
 
 (defn -main []
-  (compile 'conjure.main))
+  ;; AOT compile Conjure's source.
+  (compile 'conjure.main)
+
+  (let [ma-root "target/mranderson"]
+    ;; Map runtime deps through Mr Anderson.
+    (ma/mranderson {:clojars "https://repo.clojars.org"}
+                   (map #(with-meta % {:inline-dep true})
+                        '[[compliment "0.3.8"]])
+                   {:pname "conjure"
+                    :pversion "0.0.0"
+                    :pprefix "conjure"
+                    :srcdeps ma-root}
+                   {:src-path (io/file ma-root)
+                    :parent-clj-dirs []
+                    :branch []}))
+
+  ;; Build a list of files to load in dependency order for runtime deps.
+  )
