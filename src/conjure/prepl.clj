2b96578462a539092147445893df78f2a60c682f
Oliver Caldwell <ollie@oli.me.uk>
2019-05-30 20:32:42 +0100

    Remove the buffers from eval/read
    Fixes a weird issue where things could be evaluated while the prelude
    was still running. There were probably some super subtle race conditions
    floating about that this fixes.

--- src/conjure/prepl.clj
+++ src/conjure/prepl.clj
@@ -55,7 +55,8 @@
   channel. We can use this fact to await the read channel's closure to know
   when the closing is complete. Handy!"
   [{:keys [tag host port]}]
-  (let [[eval-chan read-chan] (repeatedly #(a/chan 32))
+  (let [eval-chan (a/chan)
+        read-chan (a/chan)
         input (PipedInputStream.)
         output (PipedOutputStream. input)]
 
@@ -111,7 +112,7 @@
   (log/info "Adding" tag host port)
   (ui/info "Adding" tag)
 
-  (let [ret-chan (a/chan 32)
+  (let [ret-chan (a/chan)
         conn {:tag tag
               :lang lang
               :host host
