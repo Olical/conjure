ea32177adccbd6cd2fed3487b647da569914d977
Oliver Caldwell <ollie@oli.me.uk>
2019-06-06 13:42:21 +0100

    Added a very simple config test

---
+++ test/conjure/config_test.clj
@@ -0,0 +1,10 @@
+(ns conjure.config-test
+  (:require [clojure.test :as t]
+            [conjure.config :as config]))
+
+(t/deftest fetch
+  (binding [config/gather (constantly {:conns {:foo {:port 5555}}})]
+    (t/is (= (config/fetch) {:conns {:foo {:port 5555
+                                           :host "127.0.0.1"
+                                           :lang :clj
+                                           :expr (#'config/default-exprs :clj)}}}))))
