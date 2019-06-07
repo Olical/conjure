780f61f971d08f46d2174e2f62f899baac3d627b
Oliver Caldwell <ollie@oli.me.uk>
2019-06-07 16:46:39 +0100

    Fix config test

--- test/conjure/config_test.clj
+++ test/conjure/config_test.clj
@@ -7,4 +7,5 @@
     (t/is (= (config/fetch) {:conns {:foo {:port 5555
                                            :host "127.0.0.1"
                                            :lang :clj
-                                           :expr (#'config/default-exprs :clj)}}}))))
+                                           :expr (#'config/default-exprs :clj)
+                                           :enabled? true}}}))))
