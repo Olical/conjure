0fdeb995daa146e1fadac56f2b9803130efa07ed
Oliver Caldwell <ollie@oli.me.uk>
2019-06-08 20:04:52 +0100

    Fix the fetch test, requires an arg

--- test/conjure/config_test.clj
+++ test/conjure/config_test.clj
@@ -4,8 +4,9 @@
 
 (t/deftest fetch
   (binding [config/gather (constantly {:conns {:foo {:port 5555}}})]
-    (t/is (= (config/fetch) {:conns {:foo {:port 5555
-                                           :host "127.0.0.1"
-                                           :lang :clj
-                                           :expr (#'config/default-exprs :clj)
-                                           :enabled? true}}}))))
+    (t/is (= (config/fetch nil)
+             {:conns {:foo {:port 5555
+                            :host "127.0.0.1"
+                            :lang :clj
+                            :expr (#'config/default-exprs :clj)
+                            :enabled? true}}}))))
