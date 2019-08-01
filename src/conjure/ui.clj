cc182522c4b9f2269b7abfea0534452365e40300
Oliver Caldwell <ollie@oli.me.uk>
2019-08-01 18:54:18 +0100

    Hooked up pretty-error

--- src/conjure/ui.clj
+++ src/conjure/ui.clj
@@ -131,157 +131,41 @@
              :kind :eval
              :msg sample})))
 
-;; Notes for pretty errors:
-; "Constructs a data representation for a Throwable with keys:
-;   :cause - root cause message
-;   :phase - error phase
-;   :via - cause chain, with cause keys:
-;            :type - exception class symbol
-;            :message - exception message
-;            :data - ex-data
-;            :at - top stack element
-;   :trace - root cause stack elements"
-;; I'll want to use clojure.lang.Compiler/demunge to parse the symbols.
-
-
-#_(do
-    (defn- pretty-error [{:keys [via trace phase cause]}]
-      (letfn [(as-comment [s]
-                (when s
-                  (util/join-lines
-                    (for [line (str/split-lines s)]
-                      (str "; " line)))))
-              (demunge [sym]
-                (Compiler/demunge (name sym)))
-              (format-stack-frame [[sym method file line]]
-                (str (demunge sym) " (" method " " file ":" line ")"))]
-        (->> (concat
-               ["; Trace..."]
-               (map-indexed
-                 (fn [n stack-frame]
-                   (str "; #" (inc n) " " (format-stack-frame stack-frame)))
-                 trace)
-               ["\n; Via..."]
-               (map-indexed
-                 (fn [n {:keys [message type data at]}]
-                   (->> (concat
-                          [(str "\n; Exception #" (inc n) " " (demunge type)
-                                (when at
-                                  (str " @ " (format-stack-frame at))))
-                           (as-comment message)
-                           (when data
-                             (util/pprint-data data))])
-                        (remove nil?)
-                        (util/join-lines)))
-                 via)
-               [(when phase
-                  (str "\n; Phase: " (name phase)))
-                (when cause
-                  (str "\n; Cause...\n"
-                       (as-comment cause)))])
-          (remove nil?)
-          (util/join-lines))))
-
-    (append {:origin :foo
-             :kind :ret
-             :code? true
-             ; :fold-text "Oh no!"
-             :msg (pretty-error '{:cause "ERROR: subquery must return only one column\n  Position: 176",
-                                  :phase :compile-syntax-check,
-                                  :trace
-                                  [[org.postgresql.core.v3.QueryExecutorImpl receiveErrorResponse
-                                    "QueryExecutorImpl.java" 2440]
-                                   [org.postgresql.core.v3.QueryExecutorImpl processResults
-                                    "QueryExecutorImpl.java" 2183]
-                                   [org.postgresql.core.v3.QueryExecutorImpl execute "QueryExecutorImpl.java"
-                                    308]
-                                   [org.postgresql.jdbc.PgStatement executeInternal "PgStatement.java" 441]
-                                   [org.postgresql.jdbc.PgStatement execute "PgStatement.java" 365]
-                                   [org.postgresql.jdbc.PgPreparedStatement executeWithFlags
-                                    "PgPreparedStatement.java" 150]
-                                   [org.postgresql.jdbc.PgPreparedStatement executeQuery
-                                    "PgPreparedStatement.java" 113]
-                                   [org.apache.commons.dbcp2.DelegatingPreparedStatement executeQuery
-                                    "DelegatingPreparedStatement.java" 122]
-                                   [org.apache.commons.dbcp2.DelegatingPreparedStatement executeQuery
-                                    "DelegatingPreparedStatement.java" 122]
-                                   [clojure.java.jdbc$execute_query_with_params invokeStatic "jdbc.clj" 1038]
-                                   [clojure.java.jdbc$execute_query_with_params invoke "jdbc.clj" 1032]
-                                   [clojure.java.jdbc$db_query_with_resultset_STAR_ invokeStatic "jdbc.clj"
-                                    1055]
-                                   [clojure.java.jdbc$db_query_with_resultset_STAR_ invoke "jdbc.clj" 1041]
-                                   [clojure.java.jdbc$query invokeStatic "jdbc.clj" 1131]
-                                   [clojure.java.jdbc$query invoke "jdbc.clj" 1093]
-                                   [clojure.lang.AFn applyToHelper "AFn.java" 160]
-                                   [clojure.lang.AFn applyTo "AFn.java" 144]
-                                   [clojure.core$apply invokeStatic "core.clj" 669]
-                                   [clojure.core$apply invoke "core.clj" 660]
-                                   [hugsql.adapter.clojure_java_jdbc.HugsqlAdapterClojureJavaJdbc query
-                                    "clojure_java_jdbc.clj" 15]
-                                   [hugsql.adapter$eval15580$fn__15643$G__15562__15648 invoke "adapter.clj" 3]
-                                   [hugsql.adapter$eval15580$fn__15643$G__15561__15654 invoke "adapter.clj" 3]
-                                   [clojure.lang.Var invoke "Var.java" 399]
-                                   [hugsql.core$db_fn_STAR_$y__15900 doInvoke "core.clj" 457]
-                                   [clojure.lang.RestFn invoke "RestFn.java" 467]
-                                   [social.interactions.comments$eval111098$fn__111103$fn__111106 invoke
-                                    "comments.clj" 112]
-                                   [social.query$with_assert_logged_in$fn__37799 invoke "query.clj" 365]
-                                   [social.query$with_coerce$fn__37561 invoke "query.clj" 120]
-                                   [social.interactions.comments$eval111098$fn__111103 invoke "comments.clj"
-                                    64] [clojure.lang.MultiFn invoke "MultiFn.java" 239]
-                                   [social.query$query_BANG_$fn__37781 invoke "query.clj" 325]
-                                   [social.query$with_tx_STAR_$fn__37521 invoke "query.clj" 71]
-                                   [clojure.java.jdbc$db_transaction_STAR_ invokeStatic "jdbc.clj" 771]
-                                   [clojure.java.jdbc$db_transaction_STAR_ invoke "jdbc.clj" 741]
-                                   [clojure.java.jdbc$db_transaction_STAR_ invokeStatic "jdbc.clj" 806]
-                                   [clojure.java.jdbc$db_transaction_STAR_ invoke "jdbc.clj" 741]
-                                   [clojure.java.jdbc$db_transaction_STAR_ invokeStatic "jdbc.clj" 754]
-                                   [clojure.java.jdbc$db_transaction_STAR_ invoke "jdbc.clj" 741]
-                                   [social.query$with_tx_STAR_ invokeStatic "query.clj" 69]
-                                   [social.query$with_tx_STAR_ invoke "query.clj" 64]
-                                   [social.query$query_BANG_ invokeStatic "query.clj" 324]
-                                   [social.query$query_BANG_ invoke "query.clj" 321]
-                                   [social.interactions.comments$eval111396 invokeStatic "comments.clj" 321]
-                                   [social.interactions.comments$eval111396 invoke "comments.clj" 321]
-                                   [clojure.lang.Compiler eval "Compiler.java" 7177]
-                                   [clojure.lang.Compiler load "Compiler.java" 7636]
-                                   [social.interactions.comments$eval111392 invokeStatic "NO_SOURCE_FILE"
-                                    30908]
-                                   [social.interactions.comments$eval111392 invoke "NO_SOURCE_FILE" 30898]
-                                   [clojure.lang.Compiler eval "Compiler.java" 7177]
-                                   [clojure.lang.Compiler eval "Compiler.java" 7167]
-                                   [clojure.lang.Compiler eval "Compiler.java" 7132]
-                                   [clojure.core$eval invokeStatic "core.clj" 3214]
-                                   [clojure.core.server$prepl$fn__8941 invoke "server.clj" 232]
-                                   [clojure.core.server$prepl invokeStatic "server.clj" 228]
-                                   [clojure.core.server$prepl doInvoke "server.clj" 191]
-                                   [clojure.lang.RestFn invoke "RestFn.java" 425]
-                                   [clojure.core.server$io_prepl invokeStatic "server.clj" 283]
-                                   [clojure.core.server$io_prepl doInvoke "server.clj" 272]
-                                   [clojure.lang.RestFn invoke "RestFn.java" 397]
-                                   [clojure.lang.AFn applyToHelper "AFn.java" 152]
-                                   [clojure.lang.RestFn applyTo "RestFn.java" 132]
-                                   [clojure.lang.Var applyTo "Var.java" 705]
-                                   [clojure.core$apply invokeStatic "core.clj" 665]
-                                   [clojure.core.server$accept_connection invokeStatic "server.clj" 73]
-                                   [clojure.core.server$start_server$fn__8879$fn__8880$fn__8882 invoke
-                                    "server.clj" 117] [clojure.lang.AFn run "AFn.java" 22]
-                                   [java.lang.Thread run "Thread.java" 835]],
-                                  :via
-                                  [{:at [clojure.lang.Compiler load "Compiler.java" 7648],
-                                    :data
-                                    #:clojure.error{:column 1,
-                                                    :line 321,
-                                                    :phase :compile-syntax-check,
-                                                    :source
-                                                    "/home/ollie/repos/weshop/platform/src/clj/social/interactions/comments.clj"},
-                                    :message
-                                    "Syntax error compiling at (/home/ollie/repos/weshop/platform/src/clj/social/interactions/comments.clj:321:1).",
-                                    :type clojure.lang.Compiler$CompilerException}
-                                   {:at [org.postgresql.core.v3.QueryExecutorImpl receiveErrorResponse
-                                         "QueryExecutorImpl.java" 2440],
-                                    :message "ERROR: subquery must return only one column\n  Position: 176",
-                                    :type org.postgresql.util.PSQLException}]})}))
+(defn- pretty-error [{:keys [via trace phase cause]}]
+  (letfn [(as-comment [s]
+            (when s
+              (util/join-lines
+                (for [line (str/split-lines s)]
+                  (str "; " line)))))
+          (demunge [sym]
+            (Compiler/demunge (name sym)))
+          (format-stack-frame [[sym method file line]]
+            (str (demunge sym) " (" method " " file ":" line ")"))]
+    (->>
+      (concat
+        [(when phase
+           (str "; Phase: " (name phase)))
+         (when cause
+           (str "; Reason...\n"
+                (as-comment cause) "\n"))]
+        (map-indexed
+          (fn [n stack-frame]
+            (str "; #" (inc n) " " (format-stack-frame stack-frame)))
+          (reverse trace))
+        (map-indexed
+          (fn [n {:keys [message type data at]}]
+            (->> (concat
+                   [(str "\n; Exception #" (inc n) " " (demunge type)
+                         (when at
+                           (str " @ " (format-stack-frame at))))
+                    (as-comment message)
+                    (when data
+                      (util/pprint-data data))])
+                 (remove nil?)
+                 (util/join-lines)))
+          via))
+      (remove nil?)
+      (util/join-lines))))
 
 (defn result
   "Format, if it's code, and display a result from an evaluation.
@@ -289,8 +173,10 @@
   [{:keys [conn resp]}]
   (let [code? (contains? #{:ret :tap} (:tag resp))
         exception? (boolean (:exception resp))
-        msg (cond-> (:val resp)
-              code? (util/pprint))]
+        msg (cond
+              exception? (pretty-error (code/parse-code (:val resp)))
+              code? (util/pprint (:val resp))
+              :else (:val resp))]
 
     (cond
       exception?
@@ -314,10 +200,10 @@
              :code? code?
              :fold-text (and code?
                              (or (when exception?
-                                   "Error folded")
+                                   "Full error folded")
                                  (when (and (= 1 (nvim/flag :fold-multiline-results))
                                             (util/multiline? msg))
-                                   "Result folded")))
+                                   "Full result folded")))
              :msg msg})))
 
 (defn load-file*
