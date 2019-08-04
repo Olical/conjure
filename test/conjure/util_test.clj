df92afb1e231b2acddc255957021b25aca5b44c3
Oliver Caldwell <ollie@oli.me.uk>
2019-08-04 22:27:14 +0100

    Correct pretty-error test regex

--- test/conjure/util_test.clj
+++ test/conjure/util_test.clj
@@ -75,10 +75,10 @@
   (t/is (= @(util/thread "adding" (+ 10 10)) 20)))
 
 (t/deftest pretty-error
-  (t/is (re-matches #"; Reason\.\.\.
+  (t/is (re-matches #"(?i); Reason\.\.\.
 ; ohno
 
-(; #\d+ [\w\./\-]+ \(\w+ [\w\.\-\_]+:\d+\)\n?)+
+(; #\d+ [\w\.\-/_\*!]+ \(\w+ [\w\.\-/_\*]+:\d+\)\n?)+
 
 ; Exception #1 java\.lang\.Error @ conjure\.util-test/fn--\d+ \(invokeStatic util_test\.clj:\d+\)
 ; ohno"
