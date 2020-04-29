25b27d65d4ecb1fdf2f84d0b91ce067706211560
Oliver Caldwell <ollie@oli.me.uk>
2020-04-29 14:08:29 +0100

    Actually make deoplete support async

--- rplugin/python3/deoplete/sources/conjure.py
+++ rplugin/python3/deoplete/sources/conjure.py
@@ -1,3 +1,4 @@
+import time
 from .base import Base
 import deoplete.logger
 
@@ -8,8 +9,14 @@ class Source(Base):
     self.vim = vim
 
     self.name = "conjure"
-    self.filetypes = vim.exec_lua("return require('conjure.config').filetypes()")
+    self.filetypes = self.lua("conjure.config", "filetypes")
     self.rank = 500
 
+  def lua(self, module, f, *args):
+    return self.vim.exec_lua("return require('" + module + "')['" + f + "'](...)", *args)
+
   def gather_candidates(self, context):
-    return self.vim.exec_lua("return require('conjure.eval')['completions-sync'](...)", context["complete_str"])
+    p = self.lua("conjure.eval", "completions-promise", context["complete_str"])
+    while not self.lua("conjure.promise", "done?", p):
+      time.sleep(0.02)
+    return self.lua("conjure.promise", "close", p)
