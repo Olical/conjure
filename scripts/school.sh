016d2870a72b6fb149ca73dbfd57259024093590
Oliver Caldwell <ollie@oli.me.uk>
2020-05-11 18:20:03 +0100

    Quiet unzip, nested autocmds

--- scripts/school.sh
+++ scripts/school.sh
@@ -5,12 +5,12 @@ PREFIX="/tmp/conjure-school"
 if [[ ! -d $PREFIX ]]; then
   echo "Downloading Conjure into $PREFIX..."
   curl -LJ https://github.com/Olical/conjure/archive/develop.zip -o $PREFIX.zip
-  unzip $PREFIX.zip -d $PREFIX
+  unzip -qq $PREFIX.zip -d $PREFIX
   rm $PREFIX.zip
 else
   echo "$PREFIX already exists, no need to download."
 fi
 
-nvim --cmd "set rtp+=$PREFIX/conjure-develop" +ConjureSchool \
+nvim --cmd "set rtp+=$PREFIX/conjure-develop" -c "au VimEnter * ++nested ConjureSchool" \
   && echo "I hope you had a great time! Cleaning up $PREFIX and exiting." \
   && rm -rf $PREFIX
