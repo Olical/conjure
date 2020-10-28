c4a654e642ec6ccc7ab89befb4e120587a5ebdfa
Oliver Caldwell <ollie@oli.me.uk>
2020-10-28 11:36:48 +0000

    Initial stdio remote work
    Actually kinda neat with libuv

--- fnl/conjure/remote/stdio.fnl
+++ fnl/conjure/remote/stdio.fnl
@@ -1,2 +1,62 @@
 (module conjure.remote.stdio
-  {require {nvim conjure.aniseed.nvim}})
+  {require {a conjure.aniseed.core
+            nvim conjure.aniseed.nvim
+            client conjure.client
+            log conjure.log}})
+
+(def- uv vim.loop)
+
+;; TODO Event hooks for success, fail, message, death.
+;; TODO All results until prompt go to the front of the callback queue.
+;; TODO Bundle msg with queue so we only send when ready (avoids stdin getting swallowed by past evals).
+;; TODO With all msgs fn for batching where required, or maybe through an opt?
+
+(defn start [opts]
+  "Starts an external REPL and gives you hooks to send code to it and read
+  responses back out. Tying an input to a result is near enough impossible
+  through this stdio medium, so it's a best effort.
+  * opts.prompt-pattern: Identify result boundaries such as '> '.
+  * opts.cmd: Command to run to start the REPL."
+  (let [stdin (uv.new_pipe false)
+        stdout (uv.new_pipe false)
+        stderr (uv.new_pipe false)]
+
+    (var repl {:queue []})
+
+    (fn destroy []
+      (stdin:shutdown))
+
+    (fn on-exit [code signal]
+      (stdin:close)
+      (stdout:close)
+      (stderr:close)
+      (repl.handle:close))
+
+    (fn on-stdout [err chunk]
+      (a.println "out:" err "-" chunk))
+
+    (fn on-stderr [err chunk]
+      (a.println "err:" err "-" chunk))
+
+    (fn send [msg cb]
+      (table.insert repl.queue cb)
+      (log.dbg "send" msg)
+      (stdin:write msg))
+
+    (let [(handle pid) (uv.spawn opts.cmd {:stdio [stdin stdout stderr]} (client.wrap on-exit))]
+      (stdout:read_start (client.wrap on-stdout))
+      (stderr:read_start (client.wrap on-stderr))
+      (a.merge!
+        repl
+        {:handle handle
+         :pid pid
+         :send send
+         :destroy destroy}))))
+
+(comment
+  (def repl (start {:prompt-pattern "> "
+                    :cmd "racket"}))
+  (repl.send "(+ 1 2)\n"
+             (fn [msg]
+               (a.println "msg:" msg)))
+  (repl.destroy))
--- lua/conjure/remote/stdio.lua
+++ lua/conjure/remote/stdio.lua
@@ -17,18 +17,71 @@ end
 local function _2_(...)
   local ok_3f_0_, val_0_ = nil, nil
   local function _2_()
-    return {require("conjure.aniseed.nvim")}
+    return {require("conjure.aniseed.core"), require("conjure.client"), require("conjure.log"), require("conjure.aniseed.nvim")}
   end
   ok_3f_0_, val_0_ = pcall(_2_)
   if ok_3f_0_ then
-    _0_0["aniseed/local-fns"] = {require = {nvim = "conjure.aniseed.nvim"}}
+    _0_0["aniseed/local-fns"] = {require = {a = "conjure.aniseed.core", client = "conjure.client", log = "conjure.log", nvim = "conjure.aniseed.nvim"}}
     return val_0_
   else
     return print(val_0_)
   end
 end
 local _1_ = _2_(...)
-local nvim = _1_[1]
+local a = _1_[1]
+local client = _1_[2]
+local log = _1_[3]
+local nvim = _1_[4]
 local _2amodule_2a = _0_0
 local _2amodule_name_2a = "conjure.remote.stdio"
-return ({nil, _0_0, {{}, nil, nil, nil}})[2]
+do local _ = ({nil, _0_0, {{}, nil, nil, nil}})[2] end
+local uv = nil
+do
+  local v_0_ = vim.loop
+  _0_0["aniseed/locals"]["uv"] = v_0_
+  uv = v_0_
+end
+local start = nil
+do
+  local v_0_ = nil
+  do
+    local v_0_0 = nil
+    local function start0(opts)
+      local stdin = uv.new_pipe(false)
+      local stdout = uv.new_pipe(false)
+      local stderr = uv.new_pipe(false)
+      local repl = {queue = {}}
+      local function destroy()
+        return stdin:shutdown()
+      end
+      local function on_exit(code, signal)
+        stdin:close()
+        stdout:close()
+        stderr:close()
+        return (repl.handle):close()
+      end
+      local function on_stdout(err, chunk)
+        return a.println("out:", err, "-", chunk)
+      end
+      local function on_stderr(err, chunk)
+        return a.println("err:", err, "-", chunk)
+      end
+      local function send(msg, cb)
+        table.insert(repl.queue, cb)
+        log.dbg("send", msg)
+        return stdin:write(msg)
+      end
+      local handle, pid = uv.spawn(opts.cmd, {stdio = {stdin, stdout, stderr}}, client.wrap(on_exit))
+      stdout:read_start(client.wrap(on_stdout))
+      stderr:read_start(client.wrap(on_stderr))
+      return a["merge!"](repl, {destroy = destroy, handle = handle, pid = pid, send = send})
+    end
+    v_0_0 = start0
+    _0_0["start"] = v_0_0
+    v_0_ = v_0_0
+  end
+  _0_0["aniseed/locals"]["start"] = v_0_
+  start = v_0_
+end
+-- (def repl (start table: 0x7f489e6c87f0)) (repl.send (+ 1 2)  (fn table: 0x7f489e4e3a88 (a.println msg: msg))) (repl.destroy)
+return nil
