142fbdeb3bd943ec77b8039d722a7a44675aa5c7
Oliver Caldwell <ollie@oli.me.uk>
2021-05-16 11:30:18 +0100

    Update Aniseed

--- lua/conjure/aniseed/fennel.lua
+++ lua/conjure/aniseed/fennel.lua
@@ -36,15 +36,48 @@ local nvim = _local_0_[1]
 local _2amodule_2a = _0_0
 local _2amodule_name_2a = "conjure.aniseed.fennel"
 do local _ = ({nil, _0_0, nil, {{}, nil, nil, nil}})[2] end
+local sync_rtp
+do
+  local v_0_
+  do
+    local v_0_0
+    local function sync_rtp0(compiler)
+      local fnl_suffix = "/fnl/?.fnl"
+      local rtp = nvim.o.runtimepath
+      local fnl_path = (rtp:gsub(",", (fnl_suffix .. ";")) .. fnl_suffix)
+      local lua_path = fnl_path:gsub("/fnl/", "/lua/")
+      compiler["path"] = (fnl_path .. ";" .. lua_path)
+      return nil
+    end
+    v_0_0 = sync_rtp0
+    _0_0["sync-rtp"] = v_0_0
+    v_0_ = v_0_0
+  end
+  local t_0_ = (_0_0)["aniseed/locals"]
+  t_0_["sync-rtp"] = v_0_
+  sync_rtp = v_0_
+end
+local state
+do
+  local v_0_ = {["compiler-loaded?"] = false}
+  local t_0_ = (_0_0)["aniseed/locals"]
+  t_0_["state"] = v_0_
+  state = v_0_
+end
 local impl
 do
   local v_0_
   do
     local v_0_0
-    local function _2_()
-      return require("conjure.aniseed.deps.fennel")
+    local function impl0()
+      local compiler = require("conjure.aniseed.deps.fennel")
+      if not state["compiler-loaded?"] then
+        state["compiler-loaded?"] = true
+        sync_rtp(compiler)
+      end
+      return compiler
     end
-    v_0_0 = _2_
+    v_0_0 = impl0
     _0_0["impl"] = v_0_0
     v_0_ = v_0_0
   end
@@ -70,25 +103,4 @@ do
   t_0_["add-path"] = v_0_
   add_path = v_0_
 end
-local sync_rtp
-do
-  local v_0_
-  do
-    local v_0_0
-    local function sync_rtp0()
-      local fnl_suffix = "/fnl/?.fnl"
-      local rtp = nvim.o.runtimepath
-      local fnl_path = (rtp:gsub(",", (fnl_suffix .. ";")) .. fnl_suffix)
-      local lua_path = fnl_path:gsub("/fnl/", "/lua/")
-      impl()["path"] = (fnl_path .. ";" .. lua_path)
-      return nil
-    end
-    v_0_0 = sync_rtp0
-    _0_0["sync-rtp"] = v_0_0
-    v_0_ = v_0_0
-  end
-  local t_0_ = (_0_0)["aniseed/locals"]
-  t_0_["sync-rtp"] = v_0_
-  sync_rtp = v_0_
-end
-return sync_rtp()
+return nil
