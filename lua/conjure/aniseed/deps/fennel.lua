cdb276bd76eff43e3702e9e0a91bf52e57608537
Oliver Caldwell <ollie@oli.me.uk>
2022-04-18 16:59:11 +0100

    Sync Aniseed / Fennel

--- lua/conjure/aniseed/deps/fennel.lua
+++ lua/conjure/aniseed/deps/fennel.lua
@@ -363,10 +363,10 @@ package.preload["conjure.aniseed.fennel.repl"] = package.preload["conjure.anisee
           _571_ = _572_
         end
       end
-      if ((_G.type(_571_) == "table") and (nil ~= (_571_).source) and (nil ~= (_571_).linedefined) and ((_571_).what == "Lua") and (nil ~= (_571_).short_src)) then
-        local source = (_571_).source
-        local line = (_571_).linedefined
+      if ((_G.type(_571_) == "table") and ((_571_).what == "Lua") and (nil ~= (_571_).short_src) and (nil ~= (_571_).linedefined) and (nil ~= (_571_).source)) then
         local src = (_571_).short_src
+        local line = (_571_).linedefined
+        local source = (_571_).source
         local fnlsrc
         do
           local t_576_ = compiler.sourcemap
