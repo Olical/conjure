ad876a8b7b527c9cf953bcd5c5844484445b3c8c
Dre Westcook <dre.westcook@octoenergy.com>
2022-08-27 17:36:45 +1000

    Seperate parser functions from client
    The client code should really be seen as a higher-level module that uses
    the transport layer to communicate with slynk.

--- fnl/conjure/client/common-lisp/slynk.fnl
+++ fnl/conjure/client/common-lisp/slynk.fnl
@@ -9,6 +9,7 @@
              config conjure.config
              client conjure.client
              remote conjure.remote.slynk
+             parser conjure.client.common-lisp.parser
              ts conjure.tree-sitter}})
 
 (def buf-suffix ".lisp")
@@ -51,7 +52,7 @@
   (with-conn-or-warn
     (fn [conn]
       (log.append
-        [(.. "; " conn.host ":" conn.port " (" status ")")]
+        [(.. "; Slynk " conn.host ":" conn.port " (" status ")")]
         {:break? true}))))
 
 (defn disconnect []
@@ -61,14 +62,6 @@
       (display-conn-status :disconnected)
       (a.assoc (state) :conn nil))))
 
-(defn- escape-string [in]
-  "puts leading slashes infront of \\ and \"
-  so that slynk can correctly interpret the results."
-  (fn replace [in pat rep]
-    (let [(s c) (string.gsub in pat rep)] s))
-  (-> in
-      (replace "\\" "\\\\")
-      (replace "\"" "\\\"")))
 
 (defn- send [msg context cb]
   (with-conn-or-warn
@@ -81,7 +74,7 @@
           conn
           (str.join
             ["(:emacs-rex (slynk:eval-and-grab-output \""
-             (escape-string msg)
+             (parser.escape-string msg)
              "\") \"" (or context ":common-lisp-user") "\" t " eval-id ")"])
           cb)))))
 
@@ -120,118 +113,11 @@
   (when (not (connected?))
     (connect {:silent? true})))
 
-(defn- string-stream [str]
-  "Convert a string into a byte-value iterator"
-  (var index 1)
-  (fn []
-    (let [r (str:byte index)]
-      (set index (+ index 1))
-      r)))
 
 (defn- display-stdout [msg]
   (when (and (not= nil msg) (not= "" msg))
     (log.append (text.prefixed-lines msg comment-prefix))))
 
-
-(defn- inner-results [received]
-  "A string of '(:return (:ok (blah)) 1)' should just give us the blah"
-  ;; this is super hacky, but it seems to work, so we're going with it
-  ;; until something better comes along.
-  (local search-string "(:return (:ok (")
-  (local tail-size 5) ;; TODO: once we fix up the increments, this will change
-  (let [(idx len) (string.find received search-string 1 true)]
-    (string.sub received
-                (+ idx len)
-                (- (string.len received) tail-size))))
-
-(defn- parse-separated-list [string-to-parse]
-  "Take a string of quoted components and return an array of those values,
-  ie: (I'm using single instead of double quotes in the example for ease)
-
-  'this is' not the 'the\' output'
-  => ['this is' 'the\' output'] (length of 2)
-
-  We must be able to correctly deal with escaped values.
-  This is the form that SWANK gives us, along the lines of:
-      (:return (:ok ('stdout-things' 'results-of-eval')) 1)
-  "
-  ;; (parse-separated-list " \"hello\" to the \"\\\"world\\\"\" ")
-  ;; expected value [ "hello" "\"world\""]
-
-  (var opened-quote nil)
-  (var escaped false)
-  (var stack [])
-  (var vals [])
-
-  (local slash-byte (string.byte "\\"))
-  (local quote-byte (string.byte "\""))
-
-  (fn maybe-insert [b]
-    "insert the value, and reset the escape flag"
-    (when opened-quote
-      (table.insert stack b)
-      (set escaped false)))
-
-  (fn maybe-close [b]
-    "When we reach a quote, we could be starting/stopping
-    a value, or we could have escaped this byte, etc"
-    (if opened-quote
-      (do
-        (when (not escaped)
-          ; move the entire stack into vals and clear
-          (set opened-quote false)
-          (table.insert
-            vals
-            (string.char
-              (unpack stack)))
-          (set stack []))
-        (when escaped
-          ;; if we've escaped this quote, put it in.
-          (maybe-insert b)))
-      (do
-        (when escaped
-          (log.dbg "Received an escaped quote outside of expected values"))
-        (set opened-quote true))))
-
-  (fn slash-escape [b]
-    "process a \\ value, which could be escaped or escaping something else"
-    (if escaped
-      (maybe-insert b)
-      (set escaped true)))
-
-  (fn dispatch [b]
-    "process each byte that comes in"
-    (match b
-      slash-byte (slash-escape b)
-      quote-byte (maybe-close b)
-      _ (maybe-insert b)))
-
-  (each [b (string-stream string-to-parse)]
-    (dispatch b))
-  ;;finally return vals
-  vals)
-
-(defn parse-result [received]
-  "Given the form (:return (:ok (\"\" \"(1 2 \\\"3\\\" 4)\")) 1) we want)])
-  to extract both
-  - the stdout, which is the first delimited quoted component
-  - the result, which is the second delimited quoted component
-
-  If there has been an error, it will not look like a result, so more parsing
-  will be needed"
-  (fn result? [response]
-    (text.starts-with response "(:return (:ok ("))
-
-  ;; TODO - parse debug messages properly and show them in a nice way
-  ;; I'm not sure what the proper conjure way is here.
-  (when (not (result? received))
-    ;;super hack; taking out the first quoted component and hoping it is
-    ;; a nice message.
-    (let [(msg) (pick-values 1 (parse-separated-list received))]
-      (display-stdout (. msg 1))))
-
-  (when (result? received)
-    (unpack (parse-separated-list (inner-results received)))))
 
 (defn eval-str [opts]
   (try-ensure-conn)
@@ -242,7 +128,7 @@
       (when (not (a.empty? opts.context))
         opts.context)
       (fn [msg]
-        (let [(stdout result) (parse-result msg)]
+        (let [(stdout result) (parser.parse-result msg)]
           (display-stdout stdout)
           (when (not= nil result)
             (when opts.on-result
--- lua/conjure/client/common-lisp/slynk.lua
+++ lua/conjure/client/common-lisp/slynk.lua
@@ -11,7 +11,7 @@ do
   _2amodule_locals_2a = (_2amodule_2a)["aniseed/locals"]
 end
 local autoload = (require("conjure.aniseed.autoload")).autoload
-local a, bridge, client, config, log, mapping, nvim, remote, str, text, ts = autoload("conjure.aniseed.core"), autoload("conjure.bridge"), autoload("conjure.client"), autoload("conjure.config"), autoload("conjure.log"), autoload("conjure.mapping"), autoload("conjure.aniseed.nvim"), autoload("conjure.remote.slynk"), autoload("conjure.aniseed.string"), autoload("conjure.text"), autoload("conjure.tree-sitter")
+local a, bridge, client, config, log, mapping, nvim, parser, remote, str, text, ts = autoload("conjure.aniseed.core"), autoload("conjure.bridge"), autoload("conjure.client"), autoload("conjure.config"), autoload("conjure.log"), autoload("conjure.mapping"), autoload("conjure.aniseed.nvim"), autoload("conjure.client.common-lisp.parser"), autoload("conjure.remote.slynk"), autoload("conjure.aniseed.string"), autoload("conjure.text"), autoload("conjure.tree-sitter")
 do end (_2amodule_locals_2a)["a"] = a
 _2amodule_locals_2a["bridge"] = bridge
 _2amodule_locals_2a["client"] = client
@@ -19,6 +19,7 @@ _2amodule_locals_2a["config"] = config
 _2amodule_locals_2a["log"] = log
 _2amodule_locals_2a["mapping"] = mapping
 _2amodule_locals_2a["nvim"] = nvim
+_2amodule_locals_2a["parser"] = parser
 _2amodule_locals_2a["remote"] = remote
 _2amodule_locals_2a["str"] = str
 _2amodule_locals_2a["text"] = text
@@ -57,7 +58,7 @@ end
 _2amodule_locals_2a["connected?"] = connected_3f
 local function display_conn_status(status)
   local function _4_(conn)
-    return log.append({("; " .. conn.host .. ":" .. conn.port .. " (" .. status .. ")")}, {["break?"] = true})
+    return log.append({("; Slynk " .. conn.host .. ":" .. conn.port .. " (" .. status .. ")")}, {["break?"] = true})
   end
   return with_conn_or_warn(_4_)
 end
@@ -71,18 +72,10 @@ local function disconnect()
   return with_conn_or_warn(_5_)
 end
 _2amodule_2a["disconnect"] = disconnect
-local function escape_string(_in)
-  local function replace(_in0, pat, rep)
-    local s, c = string.gsub(_in0, pat, rep)
-    return s
-  end
-  return replace(replace(_in, "\\", "\\\\"), "\"", "\\\"")
-end
-_2amodule_locals_2a["escape-string"] = escape_string
 local function send(msg, context, cb)
   local function _6_(conn)
     local eval_id = a.get(a.update(state(), "eval-id", a.inc), "eval-id")
-    return remote.send(conn, str.join({"(:emacs-rex (slynk:eval-and-grab-output \"", escape_string(msg), "\") \"", (context or ":common-lisp-user"), "\" t ", eval_id, ")"}), cb)
+    return remote.send(conn, str.join({"(:emacs-rex (slynk:eval-and-grab-output \"", parser["escape-string"](msg), "\") \"", (context or ":common-lisp-user"), "\" t ", eval_id, ")"}), cb)
   end
   return with_conn_or_warn(_6_)
 end
@@ -123,16 +116,6 @@ local function try_ensure_conn()
   end
 end
 _2amodule_locals_2a["try-ensure-conn"] = try_ensure_conn
-local function string_stream(str0)
-  local index = 1
-  local function _14_()
-    local r = str0:byte(index)
-    index = (index + 1)
-    return r
-  end
-  return _14_
-end
-_2amodule_locals_2a["string-stream"] = string_stream
 local function display_stdout(msg)
   if ((nil ~= msg) and ("" ~= msg)) then
     return log.append(text["prefixed-lines"](msg, comment_prefix))
@@ -141,109 +124,17 @@ local function display_stdout(msg)
   end
 end
 _2amodule_locals_2a["display-stdout"] = display_stdout
-local function inner_results(received)
-  local search_string = "(:return (:ok ("
-  local tail_size = 5
-  local idx, len = string.find(received, search_string, 1, true)
-  return string.sub(received, (idx + len), (string.len(received) - tail_size))
-end
-_2amodule_locals_2a["inner-results"] = inner_results
-local function parse_separated_list(string_to_parse)
-  local opened_quote = nil
-  local escaped = false
-  local stack = {}
-  local vals = {}
-  local slash_byte = string.byte("\\")
-  local quote_byte = string.byte("\"")
-  local function maybe_insert(b)
-    if opened_quote then
-      table.insert(stack, b)
-      escaped = false
-      return nil
-    else
-      return nil
-    end
-  end
-  local function maybe_close(b)
-    if opened_quote then
-      if not escaped then
-        opened_quote = false
-        table.insert(vals, string.char(unpack(stack)))
-        stack = {}
-      else
-      end
-      if escaped then
-        return maybe_insert(b)
-      else
-        return nil
-      end
-    else
-      if escaped then
-        log.dbg("Received an escaped quote outside of expected values")
-      else
-      end
-      opened_quote = true
-      return nil
-    end
-  end
-  local function slash_escape(b)
-    if escaped then
-      return maybe_insert(b)
-    else
-      escaped = true
-      return nil
-    end
-  end
-  local function dispatch(b)
-    local _22_ = b
-    if (_22_ == slash_byte) then
-      return slash_escape(b)
-    elseif (_22_ == quote_byte) then
-      return maybe_close(b)
-    elseif true then
-      local _ = _22_
-      return maybe_insert(b)
-    else
-      return nil
-    end
-  end
-  for b in string_stream(string_to_parse) do
-    dispatch(b)
-  end
-  return vals
-end
-_2amodule_locals_2a["parse-separated-list"] = parse_separated_list
-local function parse_result(received)
-  local function result_3f(response)
-    return text["starts-with"](response, "(:return (:ok (")
-  end
-  if not result_3f(received) then
-    local msg
-    do
-      local _24_ = parse_separated_list(received)
-      msg = _24_
-    end
-    display_stdout(msg[1])
-  else
-  end
-  if result_3f(received) then
-    return unpack(parse_separated_list(inner_results(received)))
-  else
-    return nil
-  end
-end
-_2amodule_2a["parse-result"] = parse_result
 local function eval_str(opts)
   try_ensure_conn()
   if not a["empty?"](opts.code) then
-    local _27_
+    local _15_
     if not a["empty?"](opts.context) then
-      _27_ = opts.context
+      _15_ = opts.context
     else
-      _27_ = nil
+      _15_ = nil
     end
-    local function _29_(msg)
-      local stdout, result = parse_result(msg)
+    local function _17_(msg)
+      local stdout, result = parser["parse-result"](msg)
       display_stdout(stdout)
       if (nil ~= result) then
         if opts["on-result"] then
@@ -259,7 +150,7 @@ local function eval_str(opts)
         return nil
       end
     end
-    return send(opts.code, _27_, _29_)
+    return send(opts.code, _15_, _17_)
   else
     return nil
   end
@@ -267,10 +158,10 @@ end
 _2amodule_2a["eval-str"] = eval_str
 local function doc_str(opts)
   try_ensure_conn()
-  local function _34_(_241)
+  local function _22_(_241)
     return ("(describe #'" .. _241 .. ")")
   end
-  return eval_str(a.update(opts, "code", _34_))
+  return eval_str(a.update(opts, "code", _22_))
 end
 _2amodule_2a["doc-str"] = doc_str
 local function eval_file(opts)
