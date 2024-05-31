2d63424d87f29c14593b194855efc0800b850406
Oliver Caldwell <ollie@oli.me.uk>
2024-05-31 22:24:57 +0100

    Add a script to start a local docker psql server

---
+++ dev/sql/pg-docker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+docker run \
+	--name conjure_postgres_db \
+	--rm \
+	-p 5432:5432 \
+	-e POSTGRES_USER=postgres \
+	-e POSTGRES_PASSWORD=postgres \
+	-e POSTGRES_DB=postgres \
+	postgres
