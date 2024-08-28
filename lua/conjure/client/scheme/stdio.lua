a54da440a046ad5c02552d8dc4c15e2c509b549d
Russ Tokuyama <russtoku@gmail.com>
2024-08-27 14:28:19 -1000

    Convert Scheme client

--- lua/conjure/client/scheme/stdio.lua
+++ lua/conjure/client/scheme/stdio.lua
@@ -1,52 +1,29 @@
 -- [nfnl] Compiled from fnl/conjure/client/scheme/stdio.fnl by https://github.com/Olical/nfnl, do not edit.
-local _2amodule_name_2a = "conjure.client.scheme.stdio"
-local _2amodule_2a
-do
-  _G.package.loaded[_2amodule_name_2a] = {}
-  _2amodule_2a = _G.package.loaded[_2amodule_name_2a]
-end
-local _2amodule_locals_2a
-do
-  _2amodule_2a["aniseed/locals"] = {}
-  _2amodule_locals_2a = (_2amodule_2a)["aniseed/locals"]
-end
-local autoload = (require("aniseed.autoload")).autoload
-local a, client, config, log, mapping, nvim, stdio, str, ts, _ = autoload("conjure.aniseed.core"), autoload("conjure.client"), autoload("conjure.config"), autoload("conjure.log"), autoload("conjure.mapping"), autoload("conjure.aniseed.nvim"), autoload("conjure.remote.stdio"), autoload("conjure.aniseed.string"), autoload("conjure.tree-sitter"), nil
-_2amodule_locals_2a["a"] = a
-_2amodule_locals_2a["client"] = client
-_2amodule_locals_2a["config"] = config
-_2amodule_locals_2a["log"] = log
-_2amodule_locals_2a["mapping"] = mapping
-_2amodule_locals_2a["nvim"] = nvim
-_2amodule_locals_2a["stdio"] = stdio
-_2amodule_locals_2a["str"] = str
-_2amodule_locals_2a["ts"] = ts
-_2amodule_locals_2a["_"] = _
-do local _ = {nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil} end
+local _local_1_ = require("nfnl.module")
+local autoload = _local_1_["autoload"]
+local a = autoload("conjure.aniseed.core")
+local str = autoload("conjure.aniseed.string")
+local stdio = autoload("conjure.remote.stdio")
+local config = autoload("conjure.config")
+local text = autoload("conjure.text")
+local mapping = autoload("conjure.mapping")
+local client = autoload("conjure.client")
+local log = autoload("conjure.log")
+local ts = autoload("conjure.tree-sitter")
 config.merge({client = {scheme = {stdio = {command = "mit-scheme", prompt_pattern = "[%]e][=r]r?o?r?> ", value_prefix_pattern = "^;Value: "}}}})
 if config["get-in"]({"mapping", "enable_defaults"}) then
   config.merge({client = {scheme = {stdio = {mapping = {start = "cs", stop = "cS", interrupt = "ei"}}}}})
 else
 end
 local cfg = config["get-in-fn"]({"client", "scheme", "stdio"})
-do end (_2amodule_locals_2a)["cfg"] = cfg
-do local _ = {nil, nil} end
 local state
-local function _2_()
+local function _3_()
   return {repl = nil}
 end
-state = ((_2amodule_2a).state or client["new-state"](_2_))
-do end (_2amodule_locals_2a)["state"] = state
-do local _ = {nil, nil} end
+state = client["new-state"](_3_)
 local buf_suffix = ".scm"
-_2amodule_2a["buf-suffix"] = buf_suffix
-do local _ = {nil, nil} end
 local comment_prefix = "; "
-_2amodule_2a["comment-prefix"] = comment_prefix
-do local _ = {nil, nil} end
 local form_node_3f = ts["node-surrounded-by-form-pair-chars?"]
-_2amodule_2a["form-node?"] = form_node_3f
-do local _ = {nil, nil} end
 local function with_repl_or_warn(f, opts)
   local repl = state("repl")
   if repl then
@@ -55,21 +32,17 @@ local function with_repl_or_warn(f, opts)
     return log.append({(comment_prefix .. "No REPL running")})
   end
 end
-_2amodule_locals_2a["with-repl-or-warn"] = with_repl_or_warn
-do local _ = {with_repl_or_warn, nil} end
 local function unbatch(msgs)
-  local function _4_(_241)
+  local function _5_(_241)
     return (a.get(_241, "out") or a.get(_241, "err"))
   end
-  return {out = str.join("", a.map(_4_, msgs))}
+  return {out = str.join("", a.map(_5_, msgs))}
 end
-_2amodule_2a["unbatch"] = unbatch
-do local _ = {unbatch, nil} end
 local function format_msg(msg)
-  local function _5_(_241)
+  local function _6_(_241)
     return not str["blank?"](_241)
   end
-  local function _6_(line)
+  local function _7_(line)
     if not cfg({"value_prefix_pattern"}) then
       return line
     elseif string.match(line, cfg({"value_prefix_pattern"})) then
@@ -78,33 +51,25 @@ local function format_msg(msg)
       return (comment_prefix .. "(out) " .. line)
     end
   end
-  return a.filter(_5_, a.map(_6_, str.split(string.gsub(string.gsub(a.get(msg, "out"), "^%s*", ""), "%s+%d+%s*$", ""), "\n")))
+  return a.filter(_6_, a.map(_7_, str.split(string.gsub(string.gsub(a.get(msg, "out"), "^%s*", ""), "%s+%d+%s*$", ""), "\n")))
 end
-_2amodule_2a["format-msg"] = format_msg
-do local _ = {format_msg, nil} end
 local function eval_str(opts)
-  local function _8_(repl)
-    local function _9_(msgs)
+  local function _9_(repl)
+    local function _10_(msgs)
       local msgs0 = format_msg(unbatch(msgs))
       opts["on-result"](a.last(msgs0))
       return log.append(msgs0)
     end
-    return repl.send((opts.code .. "\n"), _9_, {["batch?"] = true})
+    return repl.send((opts.code .. "\n"), _10_, {["batch?"] = true})
   end
-  return with_repl_or_warn(_8_)
+  return with_repl_or_warn(_9_)
 end
-_2amodule_2a["eval-str"] = eval_str
-do local _ = {eval_str, nil} end
 local function eval_file(opts)
   return eval_str(a.assoc(opts, "code", ("(load \"" .. opts["file-path"] .. "\")")))
 end
-_2amodule_2a["eval-file"] = eval_file
-do local _ = {eval_file, nil} end
 local function display_repl_status(status)
   return log.append({(comment_prefix .. cfg({"command"}) .. " (" .. (status or "no status") .. ")")}, {["break?"] = true})
 end
-_2amodule_locals_2a["display-repl-status"] = display_repl_status
-do local _ = {display_repl_status, nil} end
 local function stop()
   local repl = state("repl")
   if repl then
@@ -115,19 +80,17 @@ local function stop()
     return nil
   end
 end
-_2amodule_2a["stop"] = stop
-do local _ = {stop, nil} end
 local function start()
   if state("repl") then
     return log.append({(comment_prefix .. "Can't start, REPL is already running."), (comment_prefix .. "Stop the REPL with " .. config["get-in"]({"mapping", "prefix"}) .. cfg({"mapping", "stop"}))}, {["break?"] = true})
   else
-    local function _11_()
+    local function _12_()
       return display_repl_status("started")
     end
-    local function _12_(err)
+    local function _13_(err)
       return display_repl_status(err)
     end
-    local function _13_(code, signal)
+    local function _14_(code, signal)
       if (("number" == type(code)) and (code > 0)) then
         log.append({(comment_prefix .. "process exited with code " .. code)})
       else
@@ -138,38 +101,28 @@ local function start()
       end
       return stop()
     end
-    local function _16_(msg)
+    local function _17_(msg)
       return log.append(format_msg(msg))
     end
-    return a.assoc(state(), "repl", stdio.start({["prompt-pattern"] = cfg({"prompt_pattern"}), cmd = cfg({"command"}), ["on-success"] = _11_, ["on-error"] = _12_, ["on-exit"] = _13_, ["on-stray-output"] = _16_}))
+    return a.assoc(state(), "repl", stdio.start({["prompt-pattern"] = cfg({"prompt_pattern"}), cmd = cfg({"command"}), ["on-success"] = _12_, ["on-error"] = _13_, ["on-exit"] = _14_, ["on-stray-output"] = _17_}))
   end
 end
-_2amodule_2a["start"] = start
-do local _ = {start, nil} end
 local function interrupt()
-  local function _18_(repl)
+  local function _19_(repl)
     log.append({(comment_prefix .. " Sending interrupt signal.")}, {["break?"] = true})
     return repl["send-signal"](vim.loop.constants.SIGINT)
   end
-  return with_repl_or_warn(_18_)
+  return with_repl_or_warn(_19_)
 end
-_2amodule_2a["interrupt"] = interrupt
-do local _ = {interrupt, nil} end
 local function on_load()
   return start()
 end
-_2amodule_2a["on-load"] = on_load
-do local _ = {on_load, nil} end
 local function on_filetype()
   mapping.buf("SchemeStart", cfg({"mapping", "start"}), start, {desc = "Start the REPL"})
   mapping.buf("SchemeStop", cfg({"mapping", "stop"}), stop, {desc = "Stop the REPL"})
   return mapping.buf("SchemeInterrupt", cfg({"mapping", "interrupt"}), interrupt, {desc = "Interrupt the REPL"})
 end
-_2amodule_2a["on-filetype"] = on_filetype
-do local _ = {on_filetype, nil} end
 local function on_exit()
   return stop()
 end
-_2amodule_2a["on-exit"] = on_exit
-do local _ = {on_exit, nil} end
-return _2amodule_2a
+return {["buf-suffix"] = buf_suffix, ["comment-prefix"] = comment_prefix, ["form-node?"] = form_node_3f, unbatch = unbatch, ["format-msg"] = format_msg, ["eval-str"] = eval_str, ["eval-file"] = eval_file, stop = stop, start = start, interrupt = interrupt, ["on-load"] = on_load, ["on-filetype"] = on_filetype, ["on-exit"] = on_exit}
