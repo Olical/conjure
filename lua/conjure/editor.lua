e41f24148f1402c883c55334be8fbb8e33a9cc5f
Russ Tokuyama <russtoku@gmail.com>
2024-08-31 09:30:05 -1000

    Convert conjure.editor

--- lua/conjure/editor.lua
+++ lua/conjure/editor.lua
@@ -1,87 +1,53 @@
 -- [nfnl] Compiled from fnl/conjure/editor.fnl by https://github.com/Olical/nfnl, do not edit.
-local _2amodule_name_2a = "conjure.editor"
-local _2amodule_2a
-do
-  _G.package.loaded[_2amodule_name_2a] = {}
-  _2amodule_2a = _G.package.loaded[_2amodule_name_2a]
-end
-local _2amodule_locals_2a
-do
-  _2amodule_2a["aniseed/locals"] = {}
-  _2amodule_locals_2a = (_2amodule_2a)["aniseed/locals"]
-end
-local autoload = (require("aniseed.autoload")).autoload
-local a, fs, nvim, util = autoload("conjure.aniseed.core"), autoload("conjure.fs"), autoload("conjure.aniseed.nvim"), autoload("conjure.util")
-do end (_2amodule_locals_2a)["a"] = a
-_2amodule_locals_2a["fs"] = fs
-_2amodule_locals_2a["nvim"] = nvim
-_2amodule_locals_2a["util"] = util
-do local _ = {nil, nil, nil, nil, nil, nil, nil, nil, nil} end
+local _local_1_ = require("nfnl.module")
+local autoload = _local_1_["autoload"]
+local a = autoload("conjure.aniseed.core")
+local fs = autoload("conjure.fs")
+local nvim = autoload("conjure.aniseed.nvim")
+local util = autoload("conjure.util")
 local function percent_fn(total_fn)
-  local function _1_(pc)
+  local function _2_(pc)
     return math.floor(((total_fn() / 100) * (pc * 100)))
   end
-  return _1_
+  return _2_
 end
-_2amodule_locals_2a["percent-fn"] = percent_fn
-do local _ = {percent_fn, nil} end
 local function width()
-  return nvim.o.columns
+  return vim.o.columns
 end
-_2amodule_2a["width"] = width
-do local _ = {width, nil} end
 local function height()
-  return nvim.o.lines
+  return vim.o.lines
 end
-_2amodule_2a["height"] = height
-do local _ = {height, nil} end
 local percent_width = percent_fn(width)
-do end (_2amodule_2a)["percent-width"] = percent_width
-do local _ = {nil, nil} end
 local percent_height = percent_fn(height)
-do end (_2amodule_2a)["percent-height"] = percent_height
-do local _ = {nil, nil} end
 local function cursor_left()
-  return nvim.fn.screencol()
+  return vim.fn.screencol()
 end
-_2amodule_2a["cursor-left"] = cursor_left
-do local _ = {cursor_left, nil} end
 local function cursor_top()
-  return nvim.fn.screenrow()
+  return vim.fn.screenrow()
 end
-_2amodule_2a["cursor-top"] = cursor_top
-do local _ = {cursor_top, nil} end
 local function go_to(path_or_win, line, column)
   if a["string?"](path_or_win) then
     nvim.ex.edit(fs["localise-path"](path_or_win))
   else
   end
-  local _3_
+  local _4_
   if ("number" == type(path_or_win)) then
-    _3_ = path_or_win
+    _4_ = path_or_win
   else
-    _3_ = 0
+    _4_ = 0
   end
-  return nvim.win_set_cursor(_3_, {line, a.dec(column)})
+  return nvim.win_set_cursor(_4_, {line, a.dec(column)})
 end
-_2amodule_2a["go-to"] = go_to
-do local _ = {go_to, nil} end
 local function go_to_mark(m)
   return nvim.ex.normal_(("`" .. m))
 end
-_2amodule_2a["go-to-mark"] = go_to_mark
-do local _ = {go_to_mark, nil} end
 local function go_back()
   return nvim.ex.normal_(util["replace-termcodes"]("<c-o>"))
 end
-_2amodule_2a["go-back"] = go_back
-do local _ = {go_back, nil} end
 local function has_filetype_3f(ft)
-  local function _5_(_241)
+  local function _6_(_241)
     return (ft == _241)
   end
-  return a.some(_5_, nvim.fn.getcompletion(ft, "filetype"))
+  return a.some(_6_, nvim.fn.getcompletion(ft, "filetype"))
 end
-_2amodule_2a["has-filetype?"] = has_filetype_3f
-do local _ = {has_filetype_3f, nil} end
-return _2amodule_2a
+return {width = width, height = height, ["percent-width"] = percent_width, ["percent-height"] = percent_height, ["cursor-left"] = cursor_left, ["cursor-top"] = cursor_top, ["go-to"] = go_to, ["go-to-mark"] = go_to_mark, ["go-back"] = go_back, ["has-filetype?"] = has_filetype_3f}
