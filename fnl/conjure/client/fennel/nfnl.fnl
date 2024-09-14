8ee786ba9d5761a8c3b2f662460ef0f6f346fca8
Oliver Caldwell <ollie@oli.me.uk>
2024-09-14 14:53:01 +0100

    Very basic eval-str works with one shot REPLs

--- fnl/conjure/client/fennel/nfnl.fnl
+++ fnl/conjure/client/fennel/nfnl.fnl
@@ -1,6 +1,12 @@
 (local {: autoload} (require :nfnl.module))
 (local ts (autoload :conjure.tree-sitter))
 (local config (autoload :conjure.config))
+(local text (autoload :conjure.text))
+(local log (autoload :conjure.log))
+(local core (autoload :nfnl.core))
+(local fennel (autoload :nfnl.fennel))
+(local str (autoload :nfnl.string))
+(local repl (autoload :nfnl.repl))
 
 (local comment-node? ts.lisp-comment-node?)
 
@@ -25,7 +31,18 @@
 
 (local cfg (config.get-in-fn [:client :fennel :nfnl]))
 
+(fn eval-str [opts]
+  (let [eval (repl.new)
+        results (eval opts.code)
+        result-strs (core.map fennel.view results)
+        lines (text.split-lines (str.join "\n" result-strs))]
+    (log.append lines)))
+
+(comment
+  (+ 10 20))
+
 {: comment-node?
  : form-node?
  : buf-suffix
- : comment-prefix}
+ : comment-prefix
+ : eval-str}
--- lua/conjure/client/fennel/nfnl.lua
+++ lua/conjure/client/fennel/nfnl.lua
@@ -3,6 +3,12 @@ local _local_1_ = require("nfnl.module")
 local autoload = _local_1_["autoload"]
 local ts = autoload("conjure.tree-sitter")
 local config = autoload("conjure.config")
+local text = autoload("conjure.text")
+local log = autoload("conjure.log")
+local core = autoload("nfnl.core")
+local fennel = autoload("nfnl.fennel")
+local str = autoload("nfnl.string")
+local repl = autoload("nfnl.repl")
 local comment_node_3f = ts["lisp-comment-node?"]
 local function form_node_3f(node)
   return ts["node-surrounded-by-form-pair-chars?"](node, {{"#(", ")"}})
@@ -15,4 +21,12 @@ if config["get-in"]({"mapping", "enable_defaults"}) then
 else
 end
 local cfg = config["get-in-fn"]({"client", "fennel", "nfnl"})
-return {["comment-node?"] = comment_node_3f, ["form-node?"] = form_node_3f, ["buf-suffix"] = buf_suffix, ["comment-prefix"] = comment_prefix}
+local function eval_str(opts)
+  local eval = repl.new()
+  local results = eval(opts.code)
+  local result_strs = core.map(fennel.view, results)
+  local lines = text["split-lines"](str.join("\n", result_strs))
+  return log.append(lines)
+end
+--[[ (+ 10 20) ]]
+return {["comment-node?"] = comment_node_3f, ["form-node?"] = form_node_3f, ["buf-suffix"] = buf_suffix, ["comment-prefix"] = comment_prefix, ["eval-str"] = eval_str}
