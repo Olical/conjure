71298a87f205dff86de62f1a75d1a181bba31815
Oliver Caldwell <ollie@oli.me.uk>
2024-12-13 14:14:01 +0000

    Trying out a new module definition style, should play nicely with the nfnl client to allow partial reloading of values

--- fnl/conjure/client/fennel/nfnl.fnl
+++ fnl/conjure/client/fennel/nfnl.fnl
@@ -9,13 +9,13 @@
 (local repl (autoload :nfnl.repl))
 (local fs (autoload :nfnl.fs))
 
-(local comment-node? ts.lisp-comment-node?)
+(local M
+  {:comment-node? ts.lisp-comment-node?
+   :buf-suffix ".fnl"
+   :comment-prefix "; "})
 
-(fn form-node? [node]
+(fn M.form-node? [node]
   (ts.node-surrounded-by-form-pair-chars? node [["#(" ")"]]))
-
-(local buf-suffix ".fnl")
-(local comment-prefix "; ")
 
 (config.merge
   {:client
@@ -36,7 +36,7 @@
 
 ;; TODO Catch errors and display them in the log with ASCII codes removed.
 
-(fn repl-for-path [path]
+(fn M.repl-for-path [path]
   "Upserts a repl for the given path. Stored in the `repls` table.
   TODO: Add mappings or commands that allow us to reset the REPL state if they get stuck."
   (if (?. repls path)
@@ -45,7 +45,7 @@
       (tset repls path r)
       r)))
 
-(fn module-path [path]
+(fn M.module-path [path]
   "Turns a full file path into a dot.delimited.module.path. We can then use this module path to perform live reloads by modifying the currently loaded module.
 
   Finds the closest root `fnl` directory and uses that as the root of the module path.
@@ -58,11 +58,11 @@
         (str.join "." (core.rest fnl-and-below))))))
 
 (comment
-  (module-path "~/repos/Olical/conjure/fnl/conjure/client/fennel/nfnl.fnl"))
+  (M.module-path "~/repos/Olical/conjure/fnl/conjure/client/fennel/nfnl.fnl"))
 
-(fn eval-str [opts]
+(fn M.eval-str [opts]
   "Client function, called by Conjure when evaluating a string."
-  (let [repl (repl-for-path opts.file-path)
+  (let [repl (M.repl-for-path opts.file-path)
         results (repl (.. opts.code "\n"))
         result-strs (core.map fennel.view results)
         lines (text.split-lines (str.join "\n" result-strs))]
@@ -72,29 +72,21 @@
     ;; This allows you to reload a module with ef or eb.
     (when (and (or (= :buf opts.origin) (= :file opts.origin))
                (core.table? (core.last results)))
-      (let [mod-path (module-path opts.file-path)
+      (let [mod-path (M.module-path opts.file-path)
             mod (core.get package.loaded mod-path)]
         (tset package.loaded mod-path (core.merge! mod (core.last results)))))
 
     (log.append lines)))
 
-(fn eval-file [opts]
+(fn M.eval-file [opts]
   "Client function, called by Conjure when evaluating a file from disk."
   (set opts.code (core.slurp opts.file-path))
   (when opts.code
-    (eval-str opts)))
+    (M.eval-str opts)))
 
-(fn doc-str [opts]
+(fn M.doc-str [opts]
   "Client function, called by Conjure when looking up documentation."
   (core.assoc opts :code (.. ",doc " opts.code))
-  (eval-str opts))
+  (M.eval-str opts))
 
-{: comment-node?
- : form-node?
- : buf-suffix
- : module-path
- : repl-for-path
- : comment-prefix
- : eval-str
- : eval-file
- : doc-str}
+M
--- lua/conjure/client/fennel/nfnl.lua
+++ lua/conjure/client/fennel/nfnl.lua
@@ -10,12 +10,10 @@ local fennel = autoload("nfnl.fennel")
 local str = autoload("nfnl.string")
 local repl = autoload("nfnl.repl")
 local fs = autoload("nfnl.fs")
-local comment_node_3f = ts["lisp-comment-node?"]
-local function form_node_3f(node)
+local M = {["comment-node?"] = ts["lisp-comment-node?"], ["buf-suffix"] = ".fnl", ["comment-prefix"] = "; "}
+M["form-node?"] = function(node)
   return ts["node-surrounded-by-form-pair-chars?"](node, {{"#(", ")"}})
 end
-local buf_suffix = ".fnl"
-local comment_prefix = "; "
 config.merge({client = {fennel = {nfnl = {}}}})
 if config["get-in"]({"mapping", "enable_defaults"}) then
   config.merge({client = {fennel = {nfnl = {mapping = {}}}}})
@@ -23,7 +21,7 @@ else
 end
 local cfg = config["get-in-fn"]({"client", "fennel", "nfnl"})
 local repls = {}
-local function repl_for_path(path)
+M["repl-for-path"] = function(path)
   local _4_
   do
     local t_3_ = repls
@@ -41,7 +39,7 @@ local function repl_for_path(path)
     return r
   end
 end
-local function module_path(path)
+M["module-path"] = function(path)
   if path then
     local parts = fs["split-path"](fs["file-name-root"](path))
     local fnl_and_below
@@ -58,30 +56,30 @@ local function module_path(path)
     return nil
   end
 end
---[[ (module-path "~/repos/Olical/conjure/fnl/conjure/client/fennel/nfnl.fnl") ]]
-local function eval_str(opts)
-  local repl0 = repl_for_path(opts["file-path"])
+--[[ (M.module-path "~/repos/Olical/conjure/fnl/conjure/client/fennel/nfnl.fnl") ]]
+M["eval-str"] = function(opts)
+  local repl0 = M["repl-for-path"](opts["file-path"])
   local results = repl0((opts.code .. "\n"))
   local result_strs = core.map(fennel.view, results)
   local lines = text["split-lines"](str.join("\n", result_strs))
   if ((("buf" == opts.origin) or ("file" == opts.origin)) and core["table?"](core.last(results))) then
-    local mod_path = module_path(opts["file-path"])
+    local mod_path = M["module-path"](opts["file-path"])
     local mod = core.get(package.loaded, mod_path)
     package.loaded[mod_path] = core["merge!"](mod, core.last(results))
   else
   end
   return log.append(lines)
 end
-local function eval_file(opts)
+M["eval-file"] = function(opts)
   opts.code = core.slurp(opts["file-path"])
   if opts.code then
-    return eval_str(opts)
+    return M["eval-str"](opts)
   else
     return nil
   end
 end
-local function doc_str(opts)
+M["doc-str"] = function(opts)
   core.assoc(opts, "code", (",doc " .. opts.code))
-  return eval_str(opts)
+  return M["eval-str"](opts)
 end
-return {["comment-node?"] = comment_node_3f, ["form-node?"] = form_node_3f, ["buf-suffix"] = buf_suffix, ["module-path"] = module_path, ["repl-for-path"] = repl_for_path, ["comment-prefix"] = comment_prefix, ["eval-str"] = eval_str, ["eval-file"] = eval_file, ["doc-str"] = doc_str}
+return M
