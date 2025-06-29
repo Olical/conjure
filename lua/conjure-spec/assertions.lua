7493c71ab2fed886e5e25c613023ce610a66cc2c
Seamus Riordan <seamus@riordan.dev>
2025-06-29 08:25:43 -0500

    Clean up tests, rename assert-contains to assert-has-substring

--- lua/conjure-spec/assertions.lua
+++ lua/conjure-spec/assertions.lua
@@ -1,13 +1,13 @@
 -- [nfnl] fnl/conjure-spec/assertions.fnl
 local assert = require("luassert.assert")
 local say = require("say")
-local function assert_contains(_, args)
+local function assert_has_substring(_, args)
   if (string.match(args[2], args[1]) ~= nil) then
     return true
   else
     return false
   end
 end
-say:set("assertion.contains.positive", "Expected %s \nto be a substring of %s")
-say:set("assertion.contains.negative", "Expected %s \nnot to be a substring of %s")
-return assert:register("assertion", "contains", assert_contains, "assertion.contains.positive", "assertion.contains.negative")
+say:set("assertion.has-substring.positive", "Expected %s \nto be a substring of %s")
+say:set("assertion.has-substring.negative", "Expected %s \nnot to be a substring of %s")
+return assert:register("assertion", "has-substring", assert_has_substring, "assertion.has-substring.positive", "assertion.has-substring.negative")
