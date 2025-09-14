c7efe2af827c8fa3c396b87d6d7924d75b1afe22
Oliver Caldwell <ollie@oli.me.uk>
2025-09-14 14:48:11 +0100

    Migrate conjure.hook to nfnl

--- lua/conjure/hook.lua
+++ lua/conjure/hook.lua
@@ -1,25 +1,27 @@
 -- [nfnl] fnl/conjure/hook.fnl
 local _local_1_ = require("conjure.nfnl.module")
 local autoload = _local_1_["autoload"]
-local a = autoload("conjure.aniseed.core")
-local str = autoload("conjure.aniseed.string")
-local hook_fns = {}
-local hook_override_fns = {}
-local function define(name, f)
-  return a.assoc(hook_fns, name, f)
+local define = _local_1_["define"]
+local core = autoload("conjure.nfnl.core")
+local str = autoload("conjure.nfnl.string")
+local M = define("conjure.hook")
+M["hook-fns"] = (M["hook-fns"] or {})
+M["hook-override-fns"] = (M["hook-override-fns"] or {})
+M.define = function(name, f)
+  return core.assoc(M["hook-fns"], name, f)
 end
-local function override(name, f)
-  return a.assoc(hook_override_fns, name, f)
+M.override = function(name, f)
+  return core.assoc(M["hook-override-fns"], name, f)
 end
-local function get(name)
-  return a.get(hook_fns, name)
+M.get = function(name)
+  return core.get(M["hook-fns"], name)
 end
-local function exec(name, ...)
-  local f = (a.get(hook_override_fns, name) or a.get(hook_fns, name))
+M.exec = function(name, ...)
+  local f = (core.get(M["hook-override-fns"], name) or core.get(M["hook-fns"], name))
   if f then
     return f(...)
   else
     return error(str.join(" ", {"conjure.hook: Hook not found, can not exec", name}))
   end
 end
-return {["hook-fns"] = hook_fns, ["hook-override-fns"] = hook_override_fns, define = define, override = override, get = get, exec = exec}
+return M
