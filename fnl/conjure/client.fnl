ae2bb16dcf4ab3410243cf5165c6342ab8762772
Oliver Caldwell <ollie@oli.me.uk>
2025-10-03 17:23:31 +0100

    Migrate conjure.client to nfnl + maybe fix an unpack bug in the wrap function that the linter was warning me about

--- fnl/conjure/client.fnl
+++ fnl/conjure/client.fnl
@@ -1,146 +1,133 @@
-(local {: autoload} (require :conjure.nfnl.module))
-(local a (autoload :conjure.aniseed.core))
-(local nvim (autoload :conjure.aniseed.nvim))
-(local fennel (autoload :conjure.aniseed.fennel))
-(local str (autoload :conjure.aniseed.string))
+(local {: autoload : define} (require :conjure.nfnl.module))
+(local core (autoload :conjure.nfnl.core))
+(local fennel (autoload :conjure.nfnl.fennel))
+(local str (autoload :conjure.nfnl.string))
 (local config (autoload :conjure.config))
 (local dyn (autoload :conjure.dynamic))
 (local vim _G.vim)
 
-(local state-key (dyn.new #(do :default)))
+(local M (define :conjure.client))
 
-(local state
-  {:state-key-set? false})
+(set M.state-key (or M.state-key (dyn.new #(do :default))))
 
-(fn set-state-key! [new-key]
-  (set state.state-key-set? true)
-  (dyn.set-root! state-key #(do new-key)))
+(set M.state (or M.state {:state-key-set? false}))
 
-(fn multiple-states? []
-  state.state-key-set?)
+(fn M.set-state-key! [new-key]
+  (set M.state.state-key-set? true)
+  (dyn.set-root! M.state-key #(do new-key)))
 
-(fn new-state [init-fn]
+(fn M.multiple-states? []
+  M.state.state-key-set?)
+
+(fn M.new-state [init-fn]
   (let [key->state {}]
     (fn [...]
-      (let [key (state-key)
-            state (a.get key->state key)]
+      (let [key (M.state-key)
+            state (core.get key->state key)]
         (-> (if (= nil state)
-              (let [new-state (init-fn)]
-                (a.assoc key->state key new-state)
-                new-state)
-              state)
-            (a.get-in [...]))))))
+                (let [new-state (init-fn)]
+                  (core.assoc key->state key new-state)
+                  new-state)
+                state)
+            (core.get-in [...]))))))
 
 (local loaded {})
 
 (fn load-module [ft name]
-  (let [fnl (fennel.impl)
-        (ok? result) (xpcall
+  (let [(ok? result) (xpcall
                        (fn []
                          (require name))
-                       fnl.traceback)]
+                       fennel.traceback)]
 
-    (when (and ok? (a.nil? (a.get loaded name)))
-      (a.assoc loaded name
-               {:filetype ft
-                :module-name name
-                :module result})
+    (when (and ok? (core.nil? (core.get loaded name)))
+      (core.assoc loaded name
+                  {:filetype ft
+                   :module-name name
+                   :module result})
       (when (and result.on-load
-                 (not nvim.wo.diff)
+                 (not vim.wo.diff)
                  (config.get-in [:client_on_load]))
         (vim.schedule result.on-load)))
 
     (if ok?
-      result
-      (error result))))
+        result
+        (error result))))
 
-(local filetype (dyn.new #(do nvim.bo.filetype)))
-(local extension (dyn.new #(nvim.fn.expand "%:e")))
+(local filetype (dyn.new #(do vim.bo.filetype)))
+(local extension (dyn.new #(vim.fn.expand "%:e")))
 
-(fn with-filetype [ft f ...]
+(fn M.with-filetype [ft f ...]
   (dyn.bind
     {filetype #(do ft)
      extension #(do)}
     f ...))
 
-(fn wrap [f ...]
-  (let [opts {filetype (a.constantly (filetype))
-              state-key (a.constantly (state-key))}
+(fn M.wrap [f ...]
+  (let [opts {filetype (core.constantly (filetype))
+              M.state-key (core.constantly (M.state-key))}
         args [...]]
     (fn [...]
-      (if (not= 0 (a.count args))
-        (dyn.bind opts f (unpack args) ...)
-        (dyn.bind opts f ...)))))
+      (if (not= 0 (core.count args))
+          (dyn.bind opts f (unpack (core.concat args [...])))
+          (dyn.bind opts f ...)))))
 
-(fn schedule-wrap [f ...]
-  (wrap (vim.schedule_wrap f) ...))
+(fn M.schedule-wrap [f ...]
+  (M.wrap (vim.schedule_wrap f) ...))
 
-(fn schedule [f ...]
-  (vim.schedule (wrap f ...)))
+(fn M.schedule [f ...]
+  (vim.schedule (M.wrap f ...)))
 
-(fn current-client-module-name []
-  (var result {:filetype (filetype)
-               :extension (extension)
-               :module-name nil})
+(fn M.current-client-module-name []
+  (local result
+    {:filetype (filetype)
+     :extension (extension)
+     :module-name nil})
   (let [fts (when result.filetype
               (str.split result.filetype "%."))]
     (when fts
-      (for [i (a.count fts) 1 -1]
+      (for [i (core.count fts) 1 -1]
         (let [ft-part (. fts i)
               module-name (config.get-in [:filetype ft-part])
               suffixes (config.get-in [:filetype_suffixes ft-part])]
           (when (and (not result.module-name) module-name
                      (or (not suffixes)
                          (not result.extension)
-                         (a.some #(= result.extension $) suffixes)))
+                         (core.some #(= result.extension $) suffixes)))
             (set result.module-name module-name))))))
   result)
 
-(fn current []
+(fn M.current []
   (let [{: module-name : filetype : _extension}
-        (current-client-module-name)]
+        (M.current-client-module-name)]
     (when module-name
       (load-module filetype module-name))))
 
-(fn get [...]
-  (a.get-in (current) [...]))
+(fn M.get [...]
+  (core.get-in (M.current) [...]))
 
-(fn call [fn-name ...]
-  (let [f (get fn-name)]
+(fn M.call [fn-name ...]
+  (let [f (M.get fn-name)]
     (if f
-      (f ...)
+        (f ...)
 
-      (current)
-      (error (str.join
-               ["Conjure client '"
-                (a.get (current-client-module-name) :module-name)
-                "' doesn't support function: "
-                fn-name]))
+        (M.current)
+        (error (str.join
+                 ["Conjure client '"
+                  (core.get (M.current-client-module-name) :module-name)
+                  "' doesn't support function: "
+                  fn-name]))
 
-      (error "No Conjure client configured for the current file type."))))
+        (error "No Conjure client configured for the current file type."))))
 
-(fn optional-call [fn-name ...]
-  (let [f (get fn-name)]
+(fn M.optional-call [fn-name ...]
+  (let [f (M.get fn-name)]
     (when f
       (f ...))))
 
-(fn each-loaded-client [f]
-  (a.run!
+(fn M.each-loaded-client [f]
+  (core.run!
     (fn [{: filetype}]
-      (with-filetype filetype f))
-    (a.vals loaded)))
+      (M.with-filetype filetype f))
+    (core.vals loaded)))
 
-{: state-key
- : set-state-key!
- : multiple-states?
- : new-state
- : with-filetype
- : wrap
- : schedule-wrap
- : schedule
- : current-client-module-name
- : current
- : get
- : call
- : optional-call
- : each-loaded-client}
+M
--- lua/conjure/client.lua
+++ lua/conjure/client.lua
@@ -1,57 +1,60 @@
 -- [nfnl] fnl/conjure/client.fnl
 local _local_1_ = require("conjure.nfnl.module")
 local autoload = _local_1_["autoload"]
-local a = autoload("conjure.aniseed.core")
-local nvim = autoload("conjure.aniseed.nvim")
-local fennel = autoload("conjure.aniseed.fennel")
-local str = autoload("conjure.aniseed.string")
+local define = _local_1_["define"]
+local core = autoload("conjure.nfnl.core")
+local fennel = autoload("conjure.nfnl.fennel")
+local str = autoload("conjure.nfnl.string")
 local config = autoload("conjure.config")
 local dyn = autoload("conjure.dynamic")
 local vim = _G.vim
-local state_key
-local function _2_()
-  return "default"
-end
-state_key = dyn.new(_2_)
-local state = {["state-key-set?"] = false}
-local function set_state_key_21(new_key)
-  state["state-key-set?"] = true
+local M = define("conjure.client")
+local or_2_ = M["state-key"]
+if not or_2_ then
   local function _3_()
+    return "default"
+  end
+  or_2_ = dyn.new(_3_)
+end
+M["state-key"] = or_2_
+M.state = (M.state or {["state-key-set?"] = false})
+M["set-state-key!"] = function(new_key)
+  M.state["state-key-set?"] = true
+  local function _4_()
     return new_key
   end
-  return dyn["set-root!"](state_key, _3_)
+  return dyn["set-root!"](M["state-key"], _4_)
 end
-local function multiple_states_3f()
-  return state["state-key-set?"]
+M["multiple-states?"] = function()
+  return M.state["state-key-set?"]
 end
-local function new_state(init_fn)
+M["new-state"] = function(init_fn)
   local key__3estate = {}
-  local function _4_(...)
-    local key = state_key()
-    local state0 = a.get(key__3estate, key)
-    local _5_
-    if (nil == state0) then
-      local new_state0 = init_fn()
-      a.assoc(key__3estate, key, new_state0)
-      _5_ = new_state0
+  local function _5_(...)
+    local key = M["state-key"]()
+    local state = core.get(key__3estate, key)
+    local _6_
+    if (nil == state) then
+      local new_state = init_fn()
+      core.assoc(key__3estate, key, new_state)
+      _6_ = new_state
     else
-      _5_ = state0
+      _6_ = state
     end
-    return a["get-in"](_5_, {...})
+    return core["get-in"](_6_, {...})
   end
-  return _4_
+  return _5_
 end
 local loaded = {}
 local function load_module(ft, name)
-  local fnl = fennel.impl()
   local ok_3f, result = nil, nil
-  local function _8_()
+  local function _9_()
     return require(name)
   end
-  ok_3f, result = xpcall(_8_, fnl.traceback)
-  if (ok_3f and a["nil?"](a.get(loaded, name))) then
-    a.assoc(loaded, name, {filetype = ft, ["module-name"] = name, module = result})
-    if (result["on-load"] and not nvim.wo.diff and config["get-in"]({"client_on_load"})) then
+  ok_3f, result = xpcall(_9_, fennel.traceback)
+  if (ok_3f and core["nil?"](core.get(loaded, name))) then
+    core.assoc(loaded, name, {filetype = ft, ["module-name"] = name, module = result})
+    if (result["on-load"] and not vim.wo.diff and config["get-in"]({"client_on_load"})) then
       vim.schedule(result["on-load"])
     else
     end
@@ -64,42 +67,42 @@ local function load_module(ft, name)
   end
 end
 local filetype
-local function _12_()
-  return nvim.bo.filetype
-end
-filetype = dyn.new(_12_)
-local extension
 local function _13_()
-  return nvim.fn.expand("%:e")
+  return vim.bo.filetype
 end
-extension = dyn.new(_13_)
-local function with_filetype(ft, f, ...)
-  local function _14_()
+filetype = dyn.new(_13_)
+local extension
+local function _14_()
+  return vim.fn.expand("%:e")
+end
+extension = dyn.new(_14_)
+M["with-filetype"] = function(ft, f, ...)
+  local function _15_()
     return ft
   end
-  local function _15_()
+  local function _16_()
   end
-  return dyn.bind({[filetype] = _14_, [extension] = _15_}, f, ...)
+  return dyn.bind({[filetype] = _15_, [extension] = _16_}, f, ...)
 end
-local function wrap(f, ...)
-  local opts = {[filetype] = a.constantly(filetype()), [state_key] = a.constantly(state_key())}
+M.wrap = function(f, ...)
+  local opts = {[filetype] = core.constantly(filetype()), [M["state-key"]] = core.constantly(M["state-key"]())}
   local args = {...}
-  local function _16_(...)
-    if (0 ~= a.count(args)) then
-      return dyn.bind(opts, f, unpack(args), ...)
+  local function _17_(...)
+    if (0 ~= core.count(args)) then
+      return dyn.bind(opts, f, unpack(core.concat(args, {...})))
     else
       return dyn.bind(opts, f, ...)
     end
   end
-  return _16_
+  return _17_
 end
-local function schedule_wrap(f, ...)
-  return wrap(vim.schedule_wrap(f), ...)
+M["schedule-wrap"] = function(f, ...)
+  return M.wrap(vim.schedule_wrap(f), ...)
 end
-local function schedule(f, ...)
-  return vim.schedule(wrap(f, ...))
+M.schedule = function(f, ...)
+  return vim.schedule(M.wrap(f, ...))
 end
-local function current_client_module_name()
+M["current-client-module-name"] = function()
   local result = {filetype = filetype(), extension = extension(), ["module-name"] = nil}
   do
     local fts
@@ -109,22 +112,22 @@ local function current_client_module_name()
       fts = nil
     end
     if fts then
-      for i = a.count(fts), 1, -1 do
+      for i = core.count(fts), 1, -1 do
         local ft_part = fts[i]
         local module_name = config["get-in"]({"filetype", ft_part})
         local suffixes = config["get-in"]({"filetype_suffixes", ft_part})
-        local and_19_ = not result["module-name"] and module_name
-        if and_19_ then
-          local or_20_ = not suffixes or not result.extension
-          if not or_20_ then
-            local function _21_(_241)
+        local and_20_ = not result["module-name"] and module_name
+        if and_20_ then
+          local or_21_ = not suffixes or not result.extension
+          if not or_21_ then
+            local function _22_(_241)
               return (result.extension == _241)
             end
-            or_20_ = a.some(_21_, suffixes)
+            or_21_ = core.some(_22_, suffixes)
           end
-          and_19_ = or_20_
+          and_20_ = or_21_
         end
-        if and_19_ then
+        if and_20_ then
           result["module-name"] = module_name
         else
         end
@@ -134,43 +137,43 @@ local function current_client_module_name()
   end
   return result
 end
-local function current()
-  local _let_24_ = current_client_module_name()
-  local module_name = _let_24_["module-name"]
-  local filetype0 = _let_24_["filetype"]
-  local _extension = _let_24_["_extension"]
+M.current = function()
+  local _let_25_ = M["current-client-module-name"]()
+  local module_name = _let_25_["module-name"]
+  local filetype0 = _let_25_["filetype"]
+  local _extension = _let_25_["_extension"]
   if module_name then
     return load_module(filetype0, module_name)
   else
     return nil
   end
 end
-local function get(...)
-  return a["get-in"](current(), {...})
+M.get = function(...)
+  return core["get-in"](M.current(), {...})
 end
-local function call(fn_name, ...)
-  local f = get(fn_name)
+M.call = function(fn_name, ...)
+  local f = M.get(fn_name)
   if f then
     return f(...)
-  elseif current() then
-    return error(str.join({"Conjure client '", a.get(current_client_module_name(), "module-name"), "' doesn't support function: ", fn_name}))
+  elseif M.current() then
+    return error(str.join({"Conjure client '", core.get(M["current-client-module-name"](), "module-name"), "' doesn't support function: ", fn_name}))
   else
     return error("No Conjure client configured for the current file type.")
   end
 end
-local function optional_call(fn_name, ...)
-  local f = get(fn_name)
+M["optional-call"] = function(fn_name, ...)
+  local f = M.get(fn_name)
   if f then
     return f(...)
   else
     return nil
   end
 end
-local function each_loaded_client(f)
-  local function _29_(_28_)
-    local filetype0 = _28_["filetype"]
-    return with_filetype(filetype0, f)
+M["each-loaded-client"] = function(f)
+  local function _30_(_29_)
+    local filetype0 = _29_["filetype"]
+    return M["with-filetype"](filetype0, f)
   end
-  return a["run!"](_29_, a.vals(loaded))
+  return core["run!"](_30_, core.vals(loaded))
 end
-return {["state-key"] = state_key, ["set-state-key!"] = set_state_key_21, ["multiple-states?"] = multiple_states_3f, ["new-state"] = new_state, ["with-filetype"] = with_filetype, wrap = wrap, ["schedule-wrap"] = schedule_wrap, schedule = schedule, ["current-client-module-name"] = current_client_module_name, current = current, get = get, call = call, ["optional-call"] = optional_call, ["each-loaded-client"] = each_loaded_client}
+return M
